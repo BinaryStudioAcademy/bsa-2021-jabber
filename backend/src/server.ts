8ab8cd4da84435c7b42c3cb80815bb7d5131d1a4
what1s1ove <zubko.2007@gmail.com>
2021-07-10 16:25:10 +0300

    production: * build scripts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,9 +19,9 @@ app.use(urlencoded({ extended: true }));
 
 initApi(app);
 
-app.use(express.static(join(resolve(), '../public')));
+app.use(express.static(join(__dirname, '../public')));
 app.use('*', (_req, res) => {
-  return res.sendFile(join(resolve(), '../public', 'index.html'));
+  return res.sendFile(join(__dirname, '../public', 'index.html'));
 });
 
 app.use(handleError);
