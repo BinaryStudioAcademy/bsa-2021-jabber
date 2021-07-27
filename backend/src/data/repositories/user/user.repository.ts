e76c11d05029426e0d37e859f811788e357177ec
yaroslav207 <yaroslav.sprynsian8@gmail.com>
2021-07-27 22:43:11 +0300

    J11: + getByLogin in user repository

--- backend/src/data/repositories/user/user.repository.ts
+++ backend/src/data/repositories/user/user.repository.ts
@@ -19,6 +19,10 @@ class User {
   public create(payload: UserCreatePayload): Promise<TUser> {
     return this.#UserModel.query().insert(payload);
   }
+
+  public getByLogin(payload: string): Promise<TUser> {
+    return this.#UserModel.query().findOne('login', payload);
+  }
 }
 
 export { User };
