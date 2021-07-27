2bd803430a0e50f8a61256aed74e606c68c3b863
TarasMakarchuk <2278452@gmail.com>
2021-07-27 15:46:03 +0300

    Remove redux-toolkit boilerplate clutter

--- frontend/src/store/root-reducer.ts
+++ frontend/src/store/root-reducer.ts
@@ -1,9 +1,7 @@
 import { reducer as auth } from './auth/reducer';
-import { reducer as counter } from './counter/reducer';
 
 const rootReducer = {
   auth,
-  counter,
 };
 
 export { rootReducer };
