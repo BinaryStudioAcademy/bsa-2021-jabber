3d2cb6bb0df6f306b3d82e8ec930489ef39a46c3
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-07-28 20:00:50 +0300

    J-13: + create-podcasts-path

--- shared/src/common/enums/enums.ts
+++ shared/src/common/enums/enums.ts
@@ -1,4 +1,4 @@
-export { ApiPath, UsersApiPath, AuthApiPath } from './api/api';
+export { ApiPath, UsersApiPath, AuthApiPath, PodcastsApiPath } from './api/api';
 export { CustomExceptionName } from './exceptions/exceptions';
 export { ContentType } from './file/file';
 export { HttpCode, HttpHeader, HttpMethod } from './http/http';
