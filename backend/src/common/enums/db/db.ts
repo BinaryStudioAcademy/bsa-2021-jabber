ce42ef62ec53119141c2b50656f590ce9ef835c7
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-07-28 19:44:17 +0300

    J-13: + create-podcast-model

--- backend/src/common/enums/db/db.ts
+++ backend/src/common/enums/db/db.ts
@@ -1,3 +1,5 @@
 export { AbstractDTOKey } from './abstract-dto-key.enum';
 export { TableName } from './table-name.enum';
 export { UserDTOKey } from './user-dto-key.enum';
+export { PodcastDTOKey } from './podcast-dto-key.enum';
+
---
+++ backend/src/common/enums/db/podcast-dto-key.enum.ts
@@ -0,0 +1,6 @@
+enum PodcastDTOKey {
+  NAME = 'name',
+  USER_ID = 'user_id',
+}
+
+export { PodcastDTOKey };
