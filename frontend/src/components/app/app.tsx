53fc33d4856ae40d93a8834ed177dc2fdbff704b
Yevhenii <eucheremisin@gmail.com>
2021-07-28 20:59:01 +0300

    J-14: * for merge changes

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,9 +1,11 @@
 import { Header } from 'components/common/common';
+import { Toaster } from 'components/common/common';
 
 const App: React.FC = () => {
   return (
     <>
       <div className="App">
+        <Toaster />
         <Header />
       </div>
     </>
