95e8ba6b6cf3681af4849ecf3c8c60b6240360d2
what1s1ove <zubko.2007@gmail.com>
2021-07-28 22:41:19 +0300

    development: * global styles

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,14 +1,10 @@
 import { Toaster, Header } from 'components/common/common';
 
-const App: React.FC = () => {
-  return (
-    <>
-      <div className="App">
-        <Toaster />
-        <Header />
-      </div>
-    </>
-  );
-};
+const App: React.FC = () => (
+  <>
+    <Header />
+    <Toaster />
+  </>
+);
 
 export default App;
