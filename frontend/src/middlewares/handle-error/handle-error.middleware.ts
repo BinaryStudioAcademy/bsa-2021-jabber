8e780956f4c0e0045dc74c7dee234e35fa1a8111
Serhii Filonenko <35679428+Sayonarko@users.noreply.github.com>
2021-07-28 14:04:31 +0000

    J-22: * fix stylecode
    Co-authored-by: Vladyslav Zubko <60396171+ZubkoWhatislove@users.noreply.github.com>

--- frontend/src/middlewares/handle-error/handle-error.middleware.ts
+++ frontend/src/middlewares/handle-error/handle-error.middleware.ts
@@ -2,10 +2,12 @@ import { Middleware } from '@reduxjs/toolkit';
 import { notification as notificationService } from 'services/services';
 
 const handleError: Middleware = () => (next) => (action): void => {
-  const hasErrorMessage = Boolean(action.payload?.error?.message);
+  const hasErrorMessage = Boolean(action?.error?.message);
+  
   if (hasErrorMessage) {
     notificationService.error('Error', action.payload.error.message);
   }
+  
   next(action);
 };
 
