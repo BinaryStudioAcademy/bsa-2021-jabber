606f84bc37633869fe3d57563e973e7aaaa8814d
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-07-28 17:26:38 +0300

    J-22: * update handleError

--- frontend/src/middlewares/handle-error/handle-error.middleware.ts
+++ frontend/src/middlewares/handle-error/handle-error.middleware.ts
@@ -3,11 +3,11 @@ import { notification as notificationService } from 'services/services';
 
 const handleError: Middleware = () => (next) => (action): void => {
   const hasErrorMessage = Boolean(action?.error?.message);
-  
+
   if (hasErrorMessage) {
-    notificationService.error('Error', action.payload.error.message);
+    notificationService.error('Error', action.error.message);
   }
-  
+
   next(action);
 };
 
