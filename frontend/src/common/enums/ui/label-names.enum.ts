da85c4e35b31e93e02a4d26efbc94696eaad2ee6
vitalikvv <270vitalik270@gmail.com>
2021-07-29 14:47:23 +0300

    J-12: + changes labels description

--- frontend/src/common/enums/ui/label-names.enum.ts
+++ frontend/src/common/enums/ui/label-names.enum.ts
@@ -1,6 +1,6 @@
 enum LabelNames {
-  FIRST_NAME = 'First Name',
-  LAST_NAME = 'Last Name',
+  FIRST_NAME = 'First name',
+  LAST_NAME = 'Last name',
   NICKNAME = 'Nickname',
   EMAIL = 'Email',
   PASSWORD = 'Password',
