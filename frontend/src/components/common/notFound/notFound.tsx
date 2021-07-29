08354f8e7d03de28e660188a911027a4035727c4
Eugene <35200697+EugeniusGR@users.noreply.github.com>
2021-07-29 16:06:49 +0300

    J21: + router, * styles

--- frontend/src/components/common/notFound/notFound.tsx
+++ frontend/src/components/common/notFound/notFound.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 
 const NotFound: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <div className={styles.textWrapper}>
         <span className={styles.headerText}>404</span>
         <span className={styles.descriptionText}>Page not found</span>
