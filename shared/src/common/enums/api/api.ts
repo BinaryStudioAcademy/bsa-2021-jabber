f825a6a4a60c7a458b15f77c80f992059b96e893
Andrii Solohub <andrei.sologub12@gmail.com>
2021-07-29 22:42:33 +0300

    J-15: * fixed line endings

--- shared/src/common/enums/api/api.ts
+++ shared/src/common/enums/api/api.ts
@@ -2,4 +2,4 @@ export { ApiPath } from './api-path.enum';
 export { AuthApiPath } from './auth-api-path.enum';
 export { UsersApiPath } from './users-api-path.enum';
 export { PodcastsApiPath } from './podcasts-api-path.enum';
-export { EpisodesApiPath } from './episodes-api-path.enum'
+export { EpisodesApiPath } from './episodes-api-path.enum';
