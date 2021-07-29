b414ad6cfba5f8506ad1cdc33bf71e5f756b895c
Liza-Veis <liza.veis14@gmail.com>
2021-07-29 12:50:16 +0300

    J-12: * styles

--- frontend/src/components/sign-up/sign-up.tsx
+++ frontend/src/components/sign-up/sign-up.tsx
@@ -1,6 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { UserCreatePayload } from 'common/types/types';
 import {
+  AppRoute,
   ButtonType,
   DataStatus,
   InputType,
@@ -9,8 +10,9 @@ import {
 import { SignupSchema } from 'validation-schemas/validation-schemas';
 import { useAppSelector, useDispatch } from 'hooks/hooks';
 import { auth as authActions } from 'store/actions';
-import { Button, Input } from 'components/common/common';
+import { Button, Input, Link } from 'components/common/common';
 import { getResolver } from 'helpers/form/form';
+import logo from 'assets/img/logo-dark.svg';
 import styles from './styles.module.scss';
 
 const resolver = getResolver<UserCreatePayload>(SignupSchema);
@@ -33,10 +35,16 @@ const SignUp: React.FC = () => {
   return (
     <div className={styles.signUpPage}>
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.logoWrapper}>The logo</div>
-        <h3>Sign Up</h3>
-        <div>
-          Already have an account? <a href="#">Sign In</a>
+        <img
+          src={logo}
+          className={styles.formLogo}
+          loading="lazy"
+          alt="Jabber logo"
+        />
+        <h3 className={styles.formTitle}>Sign Up</h3>
+        <div className={styles.formSubtitle}>
+          Already have an account?
+          <Link to={AppRoute.SIGN_IN}>Sign In</Link>
         </div>
         <fieldset disabled={isFormDisable} className={styles.fieldset}>
           <div className={styles.formRow}>
