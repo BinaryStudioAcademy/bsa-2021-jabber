662228b41a27b2ff23047a9b6993d43f8f1d81d7
Andrii Solohub <andrei.sologub12@gmail.com>
2021-07-30 22:02:24 +0300

    J-15: + frontend service

--- frontend/src/common/types/types.ts
+++ frontend/src/common/types/types.ts
@@ -2,3 +2,4 @@ export type { RootState, AppDispatch, AsyncThunkConfig } from './app/app';
 export type { HttpOptions } from './http/http';
 export type { User, UserCreatePayload } from './user/user';
 export type { Podcast, PodcastCreatePayload } from './podcast/podcast';
+export type { Episode, EpisodeCreatePayload } from './episode/episode';
---
+++ frontend/src/services/episode-api/episode-api.service.ts
@@ -0,0 +1,55 @@
+import {
+  ApiPath,
+  // EpisodesApiPath,
+  ContentType,
+  HttpMethod,
+} from 'common/enums/enums';
+import { Http } from 'services/http/http.service';
+import { Episode, EpisodeCreatePayload } from 'common/types/types';
+
+type Constructor = {
+  http: Http;
+  apiPrefix: string;
+};
+
+class EpisodeApi {
+  #http: Http;
+  #apiPrefix: string;
+
+  constructor({ http, apiPrefix }: Constructor) {
+    this.#http = http;
+    this.#apiPrefix = apiPrefix;
+  }
+
+  public getAll(): Promise<Episode[]> {
+    return this.#http.load(
+      `${this.#apiPrefix}${ApiPath.EPISODES}`,
+      {
+        method: HttpMethod.GET,
+      },
+    );
+  }
+
+  //need to solve problem with getById
+  // public getById(id: number): Promise<Episode> {
+  //   return this.#http.load(
+  //     `${this.#apiPrefix}${ApiPath.EPISODES}${EpisodesApiPath.$ID}`,
+  //     {
+  //       method: HttpMethod.GET
+  //     },
+  //   );
+  // }
+
+  public create(payload: EpisodeCreatePayload): Promise<Episode> {
+    return this.#http.load(
+      `${this.#apiPrefix}${ApiPath.EPISODES}`,
+      {
+        method: HttpMethod.POST,
+        contentType: ContentType.JSON,
+        payload: JSON.stringify(payload),
+      },
+    );
+  }
+}
+
+export { EpisodeApi };
