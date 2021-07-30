7cc2e2a0c27ba8542dc9853a21ad84555c6a7751
vitalikvv <270vitalik270@gmail.com>
2021-07-30 15:06:49 +0300

    J-37: * changed field format, deleted redundant row

--- backend/src/data/migrations/20210730103336_add_comments_table.ts
+++ backend/src/data/migrations/20210730103336_add_comments_table.ts
@@ -3,18 +3,16 @@ import { getFormattedISODate } from '~/helpers/helpers';
 
 const TABLE_NAME = 'comments';
 
-
 export async function up(knex: Knex): Promise<void> {
   const dateNowISO = getFormattedISODate(new Date());
 
   return knex.schema.createTable(TABLE_NAME, (table) => {
     table.increments('id').primary();
-    table.string('body').notNullable();
+    table.text('text').notNullable();
     table.integer('user_id').notNullable().references('id').inTable('users');
     table.integer('episode_id').notNullable().references('id').inTable('episodes');
     table.dateTime('created_at').notNullable().defaultTo(dateNowISO);
     table.dateTime('updated_at').notNullable().defaultTo(dateNowISO);
-    table.dateTime('deleted_at').notNullable().defaultTo(dateNowISO);
   });
 }
 
