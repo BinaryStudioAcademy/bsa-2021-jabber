e4d5c2d03d7cbbe7537af7de04e39f7b010eb3b6
vitalikvv <270vitalik270@gmail.com>
2021-07-30 23:06:21 +0300

    J-38: * solving merge conflict

--- shared/src/common/enums/enums.ts
+++ shared/src/common/enums/enums.ts
@@ -1,4 +1,12 @@
-export { ApiPath, UsersApiPath, AuthApiPath, PodcastsApiPath, EpisodesApiPath, CommentsApiPath, RecordsApiPath } from './api/api';
+export {
+  ApiPath,
+  UsersApiPath,
+  AuthApiPath,
+  PodcastsApiPath,
+  EpisodesApiPath,
+  CommentsApiPath,
+  RecordsApiPath
+} from './api/api';
 export { CustomExceptionName } from './exceptions/exceptions';
 export { ContentType } from './file/file';
 export { HttpCode, HttpHeader, HttpMethod } from './http/http';
