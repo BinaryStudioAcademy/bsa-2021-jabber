f1e3555eed65c13d0315b8be823276bed6e3dfe7
JuliaDzyuba <60258687+JuliaDzyuba@users.noreply.github.com>
2021-07-30 14:47:38 +0300

    J-23: * renamed dir/file by B11, changings by comments

--- frontend/src/components/common/private-route/private-route.tsx
+++ frontend/src/components/common/private-route/private-route.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Redirect, RouteProps, RouteComponentProps, Route } from 'react-router-dom';
+import { RouteProps, RouteComponentProps } from 'react-router-dom';
+import { Redirect, Route } from 'components/common/common';
 import { AppRoute } from 'common/enums/enums';
 import { useAppSelector } from 'hooks/hooks';
 
-type IRouteProps = RouteProps & {
+type Props = RouteProps & {
   redirectTo?: AppRoute,
   component: React.ComponentType<RouteComponentProps<Record<string, string | undefined>>>,
 };
 
-const PrivateRoute: React.FC<IRouteProps> = ({ component: Component, redirectTo = AppRoute.ANY, ...otherProps }) => {
+const PrivateRoute: React.FC<Props> = ({ component: Component, redirectTo = AppRoute.SIGN_IN, ...otherProps }) => {
   const { user } = useAppSelector(({ auth }) => ({
     user: auth.user,
   }));
