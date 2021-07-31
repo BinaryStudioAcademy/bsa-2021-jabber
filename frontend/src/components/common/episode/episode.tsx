97f45d400192578e7973a813d97a5e063d956e0a
Andrii Solohub <andrei.sologub12@gmail.com>
2021-08-01 01:40:02 +0300

    J-15: + load episode

--- frontend/src/components/common/episode/episode.tsx
+++ frontend/src/components/common/episode/episode.tsx
@@ -1,24 +1,29 @@
-import { useAppSelector } from 'hooks/hooks';
+import { useAppSelector, useDispatch, useLocation, useEffect } from 'hooks/hooks';
 import { RootState } from 'common/types/types';
+import { episodepage as episodeActions } from 'store/actions';
 import styles from './styles.module.scss';
-import { store } from 'store/store';
-import { loadEpisode } from 'store/episodepage/actions';
 
 const Episode: React.FC = () => {
+  const dispatch = useDispatch();
+  const { pathname } = useLocation();
+  const episodeId = Number(pathname.split('/').pop());
+
+  useEffect(() => {
+    dispatch(episodeActions.loadEpisode(episodeId));
+  }, []);
+
   const { episode } = useAppSelector(({ episodepage }: RootState) => ({
     episode: episodepage.episode,
   }));
-
+  
   const hasEpisode = Boolean(episode);
 
-  //episodeMock should be removed
   const episodeMock = {
     podcastsGroup: 'English podcasts',
-    name: 'Episode 42',
     img: 'https://i.pinimg.com/474x/19/28/de/1928deaa3f4ba47822a5c1723f4b930f.jpg',
     description: 'Aliens send Rick, Morty and Jerry into an alternate reality, and Rick tries to get them out as oblivious Jerry pitches a marketing slogan for apples.',
   };
-
+  
   return (
     <section className={styles.episode}>
       <section className={styles.episodeHeader}>
@@ -34,25 +39,12 @@ const Episode: React.FC = () => {
             </div>
           </>
         ) : (
-          <>
-            <div className={styles.description}>
-              <h5>{episodeMock.podcastsGroup}</h5>
-              <h3>{episodeMock.name}</h3>
-              <p>
-                {episodeMock.description}
-              </p>
-            </div>
-            <div className={styles.logo}>
-              <img src={episodeMock.img} alt="logo" />
-            </div>
-          </>
+          <p>Ups, something went wrong</p>
         )
         }
       </section>
     </section>
   );
 };
-
-store.dispatch(loadEpisode(3));
 
 export default Episode;
