6d79eb4de83dfe694acada83af94f84a11b856c9
denidzio <den1k.vps2@gmail.com>
2021-08-02 12:33:27 +0300

    J-35: + set current time functionality

--- frontend/src/hooks/hooks.ts
+++ frontend/src/hooks/hooks.ts
@@ -1,4 +1,4 @@
-export { useState, useEffect } from 'react';
+export { useState, useEffect, useRef, useImperativeHandle } from 'react';
 export { useSelector, useDispatch } from 'react-redux';
 export { useHistory, useLocation, useParams } from 'react-router-dom';
 export { useAppSelector } from './app-selector/app-selector.hook';
