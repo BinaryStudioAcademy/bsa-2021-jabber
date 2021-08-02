e3331e06ad0e0df3a7492c63bc4463e8616e7a14
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-08-02 06:20:35 +0300

    J-22: * fix-service-name

--- frontend/src/store/auth/actions.ts
+++ frontend/src/store/auth/actions.ts
@@ -2,13 +2,15 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { UserCreatePayload, AsyncThunkConfig, User, UserSignInPayload } from 'common/types/types';
 import { StorageKey } from 'common/enums/enums';
 import { ActionType } from './common';
-import { storage } from 'services/services';
+import { storage as storageService } from 'services/services';
 
 const signUp = createAsyncThunk<User, UserCreatePayload, AsyncThunkConfig>
 (ActionType.SIGN_UP, async (registerPayload, { extra }) => {
   const { authApi } = extra;
   const { user, token } = await authApi.signUp(registerPayload);
-  storage.setItem(StorageKey.TOKEN, token);
+
+  storageService.setItem(StorageKey.TOKEN, token);
+
   return user;
 });
 
@@ -16,7 +18,9 @@ const signIn = createAsyncThunk<User, UserSignInPayload, AsyncThunkConfig>
 (ActionType.SIGN_IN, async (loginPayload, { extra }) => {
   const { authApi } = extra;
   const { user, token } = await authApi.signIn(loginPayload);
-  storage.setItem(StorageKey.TOKEN, token);
+
+  storageService.setItem(StorageKey.TOKEN, token);
+
   return user;
 });
 
