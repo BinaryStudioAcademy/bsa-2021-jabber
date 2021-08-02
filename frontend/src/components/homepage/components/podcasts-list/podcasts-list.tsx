a76fe22c100936a6414f6dbbefc65bce9f49fa3d
TarasMakarchuk <2278452@gmail.com>
2021-08-02 13:23:26 +0300

    J-53: * moving oops-message to homepage.tsx

--- frontend/src/components/homepage/components/podcasts-list/podcasts-list.tsx
+++ frontend/src/components/homepage/components/podcasts-list/podcasts-list.tsx
@@ -7,14 +7,12 @@ type Props = {
 };
 
 const PodcastsList: React.FC<Props> = ({ podcasts }) => {
-  const hasPodcasts = Boolean(podcasts.length);
 
   return (
     <ul className={styles.list}>
-      { hasPodcasts ? podcasts.map((it) => (
-        <Podcast podcast={it} key={it.id}/>
-      )) :
-        <span className={styles.oopsMessage}>Oops! There&apos;s nothing here</span> }
+      {podcasts.map((it) => (
+        <Podcast podcast={it} key={it.id} />
+      ))}
     </ul>
   );
 };
