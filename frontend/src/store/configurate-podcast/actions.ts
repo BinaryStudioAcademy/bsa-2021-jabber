d01ff1cec7c8f15498a198e0bc68321ef58b0879
yaroslav207 <yaroslav.sprynsian8@gmail.com>
2021-08-03 13:26:55 +0300

    J51 + reducer and action for edit podcast

--- frontend/src/store/configurate-podcast/actions.ts
+++ frontend/src/store/configurate-podcast/actions.ts
@@ -3,6 +3,7 @@ import {
   AsyncThunkConfig,
   Podcast,
   PodcastCreatePayload,
+  PodcastEditActionPayloadType,
 } from 'common/types/types';
 import { ActionType } from './common';
 
@@ -14,4 +15,20 @@ const create = createAsyncThunk<Podcast, PodcastCreatePayload, AsyncThunkConfig>
   return podcast;
 });
 
-export { create };
+const edit = createAsyncThunk<Podcast, PodcastEditActionPayloadType, AsyncThunkConfig>
+(ActionType.EDIT_PODCAST, async ( { id, payload }, { extra }) => {
+  const { podcastApi } = extra;
+  const podcast = await podcastApi.edit(id, payload);
+
+  return podcast;
+});
+
+const loadPodcast = createAsyncThunk<Podcast, number, AsyncThunkConfig>
+(ActionType.LOAD_PODCAST, async (id, { extra }) => {
+  const { podcastApi } = extra;
+  const podcast = await podcastApi.getById(id);
+
+  return podcast;
+});
+
+export { create, edit, loadPodcast };
--- frontend/src/store/configurate-podcast/reducer.ts
+++ frontend/src/store/configurate-podcast/reducer.ts
@@ -1,16 +1,18 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { DataStatus } from 'common/enums/enums';
 import { Podcast } from 'common/types/types';
-import { create } from './actions';
+import { create, edit, loadPodcast } from './actions';
 
 type State = {
   dataStatus: DataStatus;
   podcast: Podcast | null;
+  editablePodcast: Podcast | null
 };
 
 const initialState: State = {
   dataStatus: DataStatus.IDLE,
   podcast: null,
+  editablePodcast: null,
 };
 
 const reducer = createReducer(initialState, (builder) => {
@@ -22,6 +24,27 @@ const reducer = createReducer(initialState, (builder) => {
     state.podcast = action.payload;
   });
   builder.addCase(create.rejected, (state) => {
+    state.dataStatus = DataStatus.REJECTED;
+  });
+  builder.addCase(edit.pending, (state) => {
+    state.dataStatus = DataStatus.PENDING;
+  });
+  builder.addCase(edit.fulfilled, (state, action) => {
+    state.dataStatus = DataStatus.FULFILLED;
+    state.podcast = action.payload;
+    state.editablePodcast = null;
+  });
+  builder.addCase(edit.rejected, (state) => {
+    state.dataStatus = DataStatus.REJECTED;
+  });
+  builder.addCase(loadPodcast.pending, (state) => {
+    state.dataStatus = DataStatus.PENDING;
+  });
+  builder.addCase(loadPodcast.fulfilled, (state, action) => {
+    state.dataStatus = DataStatus.FULFILLED;
+    state.editablePodcast = action.payload;
+  });
+  builder.addCase(loadPodcast.rejected, (state) => {
     state.dataStatus = DataStatus.REJECTED;
   });
 });
