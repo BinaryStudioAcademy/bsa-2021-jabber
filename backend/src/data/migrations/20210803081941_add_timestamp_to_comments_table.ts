a6ddc1f1d68c19160b35fc3064294da712b70193
Yevhenii <eucheremisin@gmail.com>
2021-08-03 11:30:04 +0300

    J73: + migration timestamp to the comments

---
+++ backend/src/data/migrations/20210803081941_add_timestamp_to_comments_table.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex';
+
+const TABLE_NAME = 'comments';
+const COLUMN_NAME = 'timestamp';
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.integer(COLUMN_NAME).notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME);
+  });
+}
