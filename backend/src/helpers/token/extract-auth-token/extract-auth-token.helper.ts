fd5e56a2b0a71bbb04984bb55cdc01e62aa1887f
denidzio <den1k.vps2@gmail.com>
2021-08-05 20:41:37 +0300

    J-75: * type of extractAuthToken function argument

--- backend/src/helpers/token/extract-auth-token/extract-auth-token.helper.ts
+++ backend/src/helpers/token/extract-auth-token/extract-auth-token.helper.ts
@@ -1,4 +1,4 @@
-const extractAuthToken = (header: string | undefined): string | null => {
+const extractAuthToken = (header?: string): string | null => {
   const [, token] = String(header).split(' ');
   return token ?? null;
 };
