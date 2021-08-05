d30c93a3d2e3a845525d75e6a210091e6b828993
denidzio <den1k.vps2@gmail.com>
2021-08-05 09:56:30 +0300

    J-75: + current user id as trace id in logger

--- backend/src/middlewares/set-trace-id/set-trace-id.middleware.ts
+++ backend/src/middlewares/set-trace-id/set-trace-id.middleware.ts
@@ -1,10 +1,18 @@
 import { RequestHandler } from 'express';
-import { appAsyncStorage } from '~/services/services';
+import { appAsyncStorage, token } from '~/services/services';
 import { getRandomId } from '~/helpers/helpers';
 import { AppAsyncStorageKey } from '~/common/enums/enums';
+import { TokenPayload } from '~/common/types/types';
 
-const setTraceId: RequestHandler = async (_req, _res, next) => {
-  const traceId = getRandomId();
+const setTraceId: RequestHandler = async (req, _res, next) => {
+  const authorizationHeader = req.get('Authorization');
+  const bearerToken = authorizationHeader?.split(' ')[1];
+
+  const payload: TokenPayload | null = bearerToken
+    ? token.decode(bearerToken)
+    : null;
+
+  const traceId = payload ? payload.userId : getRandomId();
   const store = new Map().set(AppAsyncStorageKey.TRACE_ID, traceId);
 
   return appAsyncStorage.run(store, () => {
