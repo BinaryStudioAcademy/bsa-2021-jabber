691b1075c7ff88e2a67ac097a765455e8ab1550f
vitalikvv <270vitalik270@gmail.com>
2021-08-05 23:32:44 +0300

    J78: + white routes for public routes

--- backend/src/common/constants/routes/routes-white-list.ts
+++ backend/src/common/constants/routes/routes-white-list.ts
@@ -1,6 +1,6 @@
 import { AuthApiPath, ApiPath, PodcastsApiPath, EpisodesApiPath, HttpMethod } from '~/common/enums/enums';
 
-let ROUTES_WHITE_LIST = new Map([
+const ROUTES_WHITE_LIST = new Map([
   [`${ApiPath.AUTH}${AuthApiPath.SIGN_IN}`, [HttpMethod.GET, HttpMethod.POST]],
   [`${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`, [HttpMethod.GET, HttpMethod.POST]],
   [`${ApiPath.PODCASTS}${PodcastsApiPath.ROOT}`, [HttpMethod.GET]],
