abb082b0630e80a3734de9348db05d1131f2f306
denidzio <den1k.vps2@gmail.com>
2021-08-05 12:04:50 +0300

    J-75: * extracting authorization header way

--- backend/src/middlewares/set-trace-id/set-trace-id.middleware.ts
+++ backend/src/middlewares/set-trace-id/set-trace-id.middleware.ts
@@ -5,14 +5,9 @@ import { AppAsyncStorageKey } from '~/common/enums/enums';
 import { TokenPayload } from '~/common/types/types';
 
 const setTraceId: RequestHandler = async (req, _res, next) => {
-  const authorizationHeader = req.get('Authorization');
-  const bearerToken = authorizationHeader?.split(' ')[1];
-
-  const payload: TokenPayload | null = bearerToken
-    ? token.decode(bearerToken)
-    : null;
-
-  const traceId = payload ? payload.userId : getRandomId();
+  const [, bearerToken] = <string[]>req.headers.authorization?.split(' ');
+  const payload = token.decode<TokenPayload | null>(bearerToken);
+  const traceId = payload?.userId ?? getRandomId();
   const store = new Map().set(AppAsyncStorageKey.TRACE_ID, traceId);
 
   return appAsyncStorage.run(store, () => {
