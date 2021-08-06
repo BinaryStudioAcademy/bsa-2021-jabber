8691867746c5c6d323ccd1da65892dcc94ca727e
vitalikvv <270vitalik270@gmail.com>
2021-08-06 17:53:11 +0300

    J78: * fixed linter error

--- backend/src/helpers/whiteroutes/checkWhiteRoutes.helper.ts
+++ backend/src/helpers/whiteroutes/checkWhiteRoutes.helper.ts
@@ -1,30 +1,30 @@
 import { WhiteRoute } from '~/common/types/types';
 
-const checkIsRouteWhite = (splitedWhiteRoute: string[], splitedRequestRoute: string[]) => {
+const checkIsRouteWhite = (splitedWhiteRoute: string[], splitedRequestRoute: string[]):boolean => {
   let isRouteWhite = false;
-  splitedWhiteRoute.forEach(whiteRouteItem => {
-    if (splitedRequestRoute.indexOf(whiteRouteItem) !== -1) isRouteWhite = true
-  })
+  splitedWhiteRoute.forEach((whiteRouteItem) => {
+    if (splitedRequestRoute.indexOf(whiteRouteItem) !== -1) isRouteWhite = true;
+  });
   return isRouteWhite;
-}
+};
 
-const checkIsMethodAllowed = (allowedMethods: string[], requestMethod: string) => {
-  return allowedMethods.some((method) => method === requestMethod)
-}
+const checkIsMethodAllowed = (allowedMethods: string[], requestMethod: string):boolean => {
+  return allowedMethods.some((method) => method === requestMethod);
+};
 
 const checkUserAccess = (
   routesWhiteList:WhiteRoute[],
   requestRoute: string,
-  requestMethod: string
-):Boolean => {
-  const splitedRequestRoute = requestRoute.split('/').filter(e => e);
+  requestMethod: string,
+):boolean => {
+  const splitedRequestRoute = requestRoute.split('/').filter((e) => e);
   for (let i = 0; i < routesWhiteList.length; i++) {
-    const splitedWhiteRoute = routesWhiteList[i].path.split('/').filter(e => e);
-    let isRouteWhite = checkIsRouteWhite(splitedWhiteRoute, splitedRequestRoute);
-    let isMethodAllowed = checkIsMethodAllowed(routesWhiteList[i].allowedMethods, requestMethod)
+    const splitedWhiteRoute = routesWhiteList[i].path.split('/').filter((e) => e);
+    const isRouteWhite = checkIsRouteWhite(splitedWhiteRoute, splitedRequestRoute);
+    const isMethodAllowed = checkIsMethodAllowed(routesWhiteList[i].allowedMethods, requestMethod);
     if (isRouteWhite && isMethodAllowed) return true;
   }
   return false;
-}
+};
 
-export { checkUserAccess }
+export { checkUserAccess };
