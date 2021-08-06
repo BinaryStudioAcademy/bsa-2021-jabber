d96213ca061ad8ed4090e2d328e2e5b36846691a
Yevhenii <eucheremisin@gmail.com>
2021-08-06 11:47:20 +0300

    J83: + default image

--- frontend/src/components/homepage/components/podcast/podcast.tsx
+++ frontend/src/components/homepage/components/podcast/podcast.tsx
@@ -2,6 +2,7 @@ import { AppRoute } from 'common/enums/enums';
 import { Podcast as TPodcast } from 'common/types/types';
 import { Link } from 'components/common/common';
 import styles from './styles.module.scss';
+import DEFAULT_IMAGE from 'assets/img/default_podcast_image.jpeg';
 
 type Props = {
   podcast: TPodcast;
@@ -11,7 +12,7 @@ const Podcast: React.FC<Props> = ({ podcast }) => (
   <li className={styles.wrapper}>
     <div className={styles.imageWrapper}>
       <img
-        src={podcast.image?.url}
+        src={podcast.image?.url ? podcast.image?.url : DEFAULT_IMAGE}
         className={styles.podcastImage}
         width="195"
         height="195"
