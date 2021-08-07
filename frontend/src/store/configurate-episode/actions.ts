0d37aa94695c6db9b28f9425031cf10a12d85da2
yaroslav207 <yaroslav.sprynsian8@gmail.com>
2021-08-08 00:00:31 +0300

    J86 * action create episode for upload record

--- frontend/src/store/configurate-episode/actions.ts
+++ frontend/src/store/configurate-episode/actions.ts
@@ -2,18 +2,24 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import {
   Episode,
   AsyncThunkConfig,
-  EpisodeCreatePayload,
+  EpisodeFormPayload,
   User,
 } from 'common/types/types';
 import { ActionType } from './common';
+import { getFileFromFileList, getDataUrl } from 'helpers/helpers';
 
-const createEpisode = createAsyncThunk<Episode, EpisodeCreatePayload, AsyncThunkConfig>
+const createEpisode = createAsyncThunk<Episode, EpisodeFormPayload, AsyncThunkConfig>
 (ActionType.CREATE_EPISODE, async (createEpisodePayload, { getState, extra }) => {
   const { episodeApi } = extra;
   const { auth } = getState();
+  const file = getFileFromFileList(createEpisodePayload.record);
   const episodes = await episodeApi.create({
-    ...createEpisodePayload,
+    name: createEpisodePayload.name,
+    description: createEpisodePayload.description,
+    podcastId: createEpisodePayload.podcastId,
+    type: createEpisodePayload.type,
     userId: (<User>auth.user).id,
+    recordDataUrl: file ? await getDataUrl(file) : null,
   });
 
   return episodes;
