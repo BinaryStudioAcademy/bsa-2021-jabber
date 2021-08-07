2776ecc7d48b9ff7c184a7d090420c2fa35ada54
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-08-07 16:11:54 +0300

    update-podcast-validation-schema

--- frontend/src/validation-schemas/podcast/create-podcast/create-podcast.validation-schema.ts
+++ frontend/src/validation-schemas/podcast/create-podcast/create-podcast.validation-schema.ts
@@ -1,12 +1,9 @@
 import { Joi } from 'helpers/helpers';
 import { podcast } from 'jabber-shared/validation-schemas/validation-schemas';
-import { PodcastPayloadKey, PodcastType } from 'common/enums/enums';
-
-const podcastType = Object.values(PodcastType);
+import { PodcastPayloadKey } from 'common/enums/enums';
 
 const podcastCreate = podcast.keys({
   [PodcastPayloadKey.IMAGE]: Joi.object().required(),
-  [PodcastPayloadKey.TYPE]: Joi.string().valid(...podcastType),
 });
 
 export { podcastCreate };
