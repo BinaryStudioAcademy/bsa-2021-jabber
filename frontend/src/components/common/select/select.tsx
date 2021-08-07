76bc762f226caae34f8c2fa98c0f9901ccf5caa8
Yevhenii <eucheremisin@gmail.com>
2021-08-07 23:46:48 +0300

    J70: + controller

--- frontend/src/components/common/select/select.tsx
+++ frontend/src/components/common/select/select.tsx
@@ -2,6 +2,7 @@ import SelectReact from 'react-select';
 import { Option } from 'common/types/types';
 import { ErrorMessage } from '@hookform/error-message';
 import { Control, FieldErrors, Path, FieldValues } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
 import styles from './styles.module.scss';
 
 type Props = {
@@ -17,13 +18,14 @@ const Select: React.FC<Props> = ({ options, label, name, control, errors }) => {
   return (
     <label className={styles.inputWrapper}>
       <span className={styles.label}>{label}</span>
-      <SelectReact
-        control={control}
-        options={options}
-        className={styles.input}
-        isSearchable={false}
+      <Controller
         name={name}
+        control={control}
+        render={({ field }): any => (
+          <SelectReact {...field} options={options} />
+        )}
       />
+
       <span className={styles.errorWrapper}>
         <ErrorMessage errors={errors} name={name} />
       </span>
