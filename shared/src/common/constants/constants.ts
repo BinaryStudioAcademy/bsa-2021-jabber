ca0b0ac484c31bad505769fb3315faafbeab60e1
denidzio <den1k.vps2@gmail.com>
2021-08-08 10:45:44 +0300

    J-87: + default episode id

--- shared/src/common/constants/constants.ts
+++ shared/src/common/constants/constants.ts
@@ -1,3 +1,4 @@
 export { DEFAULT_USER_ID } from './user/user';
 export { DEFAULT_PODCAST_ID } from './podcast/podcast';
 export { FIRST_ARRAY_IDX } from './array/array';
+export { DEFAULT_EPISODE_ID } from './episode/episode';
---
+++ shared/src/common/constants/episode/episode.ts
@@ -0,0 +1,3 @@
+const DEFAULT_EPISODE_ID = -1;
+
+export { DEFAULT_EPISODE_ID };
