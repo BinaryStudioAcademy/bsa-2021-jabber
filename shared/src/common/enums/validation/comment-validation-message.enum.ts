3431577a3850685d280071fc8d3693fb65d2cf67
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-08-08 11:54:03 +0300

    J-65: * fix-error-message

--- shared/src/common/enums/validation/comment-validation-message.enum.ts
+++ shared/src/common/enums/validation/comment-validation-message.enum.ts
@@ -5,8 +5,8 @@ const CommentValidationMessage = {
   COMMENT_TEXT_MIN_LENGTH: `Comment text must be at least ${CommentValidationRule.COMMENT_TEXT_MIN_LENGTH} character long`,
   USER_ID_REQUIRE: 'User id is required',
   USER_ID_NUMBER_FORMAT: 'User id must be an integer',
-  EPISODE_ID_REQUIRE: 'Podcast id is required',
-  EPISODE_ID_NUMBER_FORMAT: 'Podcast id must be an integer',
+  EPISODE_ID_REQUIRE: 'Episode id is required',
+  EPISODE_ID_NUMBER_FORMAT: 'Episode id must be an integer',
 } as const;
 
 export { CommentValidationMessage };
