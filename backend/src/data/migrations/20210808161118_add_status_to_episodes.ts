7a530f62cb5025678502761c6d9e6e7e103a84ea
yaroslav207 <yaroslav.sprynsian8@gmail.com>
2021-08-08 19:27:25 +0300

    J93: + migration add status to episodes

---
+++ backend/src/data/migrations/20210808161118_add_status_to_episodes.ts
@@ -0,0 +1,24 @@
+import { Knex } from "knex";
+
+const TABLE_NAME = 'episodes';
+const COLUMN_NAME = 'status';
+
+const EpisodeStatus = {
+  PUBLISHED: 'published',
+  STAGING: 'staging',
+} as const;
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table
+      .enum(COLUMN_NAME, [EpisodeStatus.PUBLISHED, EpisodeStatus.STAGING])
+      .defaultTo(EpisodeStatus.PUBLISHED);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME);
+  });
+}
+
