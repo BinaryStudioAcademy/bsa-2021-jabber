dee6bd83f94766585d97b0552dc44eb1a82c5d45
Now111 <denys.kyznietsov@binary-studio.com>
2021-08-08 16:45:18 +0300

    init postman tests

---
+++ __tests__/start.all.sh
@@ -0,0 +1,11 @@
+npm run install:shared
+
+npm run build:shared
+
+docker build -t backend -f ./__tests__/docker/Backend.Dockerfile .
+
+docker-compose -f ./__tests__/docker-compose.tests.yml up -d
+
+sleep 35
+
+npm run test:api
