6decbb24b0cf2e6f38e553fe3d74d3f4ac57b99c
Andrii Solohub <andrei.sologub12@gmail.com>
2021-08-08 17:25:04 +0300

    J-67: *  past episode rout

--- frontend/src/components/configurate-episode/components/create-episode-form/common/constants.ts
+++ frontend/src/components/configurate-episode/components/create-episode-form/common/constants.ts
@@ -1,12 +1,9 @@
 import { EpisodePayloadKey, EpisodeType } from 'common/enums/enums';
 import { EpisodeFormPayload } from 'common/types/types';
-// import { DEFAULT_PODCAST_ID } from 'common/constants/constants';
 
 const DEFAULT_CREATE_EPISODE_PAYLOAD: EpisodeFormPayload = {
   [EpisodePayloadKey.NAME]: '',
   [EpisodePayloadKey.DESCRIPTION]: '',
-  // [EpisodePayloadKey.USER_ID]: DEFAULT_USER_ID,
-  // [EpisodePayloadKey.PODCAST_ID]: DEFAULT_PODCAST_ID,
   [EpisodePayloadKey.TYPE]: EpisodeType.PUBLIC,
   [EpisodePayloadKey.IMAGE]: null,
 };
