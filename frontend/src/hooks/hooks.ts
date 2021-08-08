cff683a17ac4ea5f60c808d02e59f0874d0ed07f
denidzio <den1k.vps2@gmail.com>
2021-08-08 10:47:17 +0300

    J-87: + useFieldArray to hooks

--- frontend/src/hooks/hooks.ts
+++ frontend/src/hooks/hooks.ts
@@ -1,6 +1,6 @@
 export { useState, useEffect, useRef, useCallback } from 'react';
 export { useSelector, useDispatch } from 'react-redux';
-export { useForm } from 'react-hook-form';
+export { useForm, useFieldArray } from 'react-hook-form';
 export { useHistory, useLocation, useParams } from 'react-router-dom';
 export { useAppSelector } from './app-selector/app-selector.hook';
 export { useAppForm } from './app-form/app-form.hook';
