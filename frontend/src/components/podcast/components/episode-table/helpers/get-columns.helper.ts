55950768c64e7f3df07695f95e576b518a0cd0b3
Yevhenii <eucheremisin@gmail.com>
2021-08-09 15:54:01 +0300

    J100: * hot-fixes

--- frontend/src/components/podcast/components/episode-table/helpers/get-columns.helper.ts
+++ frontend/src/components/podcast/components/episode-table/helpers/get-columns.helper.ts
@@ -1,12 +1,13 @@
 import { Column } from 'react-table';
-import { INCREASE_CONT_FOR_IDX } from 'jabber-shared/common/constants/constants';
+import { INCREASE_CONT_FOR_IDX } from 'common/constants/constants';
 
 const getColumns = (): Column[] => {
   const columns: Column[] = [
     {
       Header: '#',
-      accessor: (_originalRow, rowIndex): number =>
-        rowIndex + INCREASE_CONT_FOR_IDX,
+      accessor: (_originalRow, rowIndex): number => {
+        return rowIndex + INCREASE_CONT_FOR_IDX;
+      },
     },
     {
       Header: 'Name and Host',
