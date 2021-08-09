d156fb5dd8c66b1486be376cc043edd771f1ae61
Andrii Solohub <andrei.sologub12@gmail.com>
2021-08-09 11:14:27 +0300

    J-67: * got back default value

--- frontend/src/store/configurate-episode/actions.ts
+++ frontend/src/store/configurate-episode/actions.ts
@@ -7,7 +7,7 @@ import {
 } from 'common/types/types';
 import { getFileFromFileList, getDataUrl } from 'helpers/helpers';
 import { ActionType } from './common';
-// import { DEFAULT_PODCAST_ID } from 'common/constants/constants';
+import { DEFAULT_PODCAST_ID } from 'common/constants/constants';
 
 const createEpisode = createAsyncThunk<Episode, EpisodeFormPayload, AsyncThunkConfig>
 (ActionType.CREATE_EPISODE, async (createEpisodePayload, { getState, extra }) => {
@@ -20,7 +20,7 @@ const createEpisode = createAsyncThunk<Episode, EpisodeFormPayload, AsyncThunkCo
   const episodes = await episodeApi.create({
     name: createEpisodePayload.name,
     description: createEpisodePayload.description,
-    podcastId: 2,
+    podcastId: DEFAULT_PODCAST_ID,
     type: createEpisodePayload.type,
     userId: (<User>auth.user).id,
     recordDataUrl: file ? await getDataUrl(file) : null,
