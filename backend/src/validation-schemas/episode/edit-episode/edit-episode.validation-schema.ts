9d0929a773f6788da9203dcf0ba86cc1b13d0f2e
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-08-09 16:32:16 +0300

    J-56: * validation-schema

--- backend/src/validation-schemas/episode/edit-episode/edit-episode.validation-schema.ts
+++ backend/src/validation-schemas/episode/edit-episode/edit-episode.validation-schema.ts
@@ -6,6 +6,10 @@ import {
 } from '~/common/enums/enums';
 
 const episodeEdit = episode.keys({
+  [EpisodePayloadKey.USER_ID]: Joi.number().integer().required().messages({
+    'number.required': EpisodeValidationMessage.USER_ID_REQUIRE,
+    'number.integer': EpisodeValidationMessage.USER_ID_NUMBER_FORMAT,
+  }),
   [EpisodePayloadKey.RECORD_DATA_URL]: [
     Joi.string().uri().messages({
       'string.uri': EpisodeValidationMessage.DATA_URL_FORMAT,
