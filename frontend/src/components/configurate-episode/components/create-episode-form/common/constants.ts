2cea78bb63f5cac26ed645344c8d5d30d95e185d
denidzio <den1k.vps2@gmail.com>
2021-08-09 09:50:37 +0300

    J-87: * typed DEFAULT_SHOWNOTE_PAYLOAD

--- frontend/src/components/configurate-episode/components/create-episode-form/common/constants.ts
+++ frontend/src/components/configurate-episode/components/create-episode-form/common/constants.ts
@@ -1,10 +1,13 @@
-import { EpisodeCreatePayloadKey, EpisodeType } from 'common/enums/enums';
-import { EpisodeCreatePayload } from 'common/types/types';
+import { EpisodeCreatePayload, ShownotePayload } from 'common/types/types';
+import {
+  EpisodeCreatePayloadKey,
+  EpisodeType,
+  ShownotePayloadKey,
+} from 'common/enums/enums';
 import {
   DEFAULT_USER_ID,
   DEFAULT_PODCAST_ID,
 } from 'common/constants/constants';
-import { ShownoteCreatePayloadKey } from 'jabber-shared/common/enums/enums';
 
 const DEFAULT_CREATE_EPISODE_PAYLOAD: EpisodeCreatePayload = {
   [EpisodeCreatePayloadKey.NAME]: '',
@@ -15,9 +18,9 @@ const DEFAULT_CREATE_EPISODE_PAYLOAD: EpisodeCreatePayload = {
   [EpisodeCreatePayloadKey.TYPE]: EpisodeType.PUBLIC,
 };
 
-const DEFAULT_CREATE_SHOWNOTE_PAYLOAD = {
-  [ShownoteCreatePayloadKey.NAME]: '',
-  [ShownoteCreatePayloadKey.TIMESTAMP]: 0,
+const DEFAULT_SHOWNOTE_PAYLOAD: ShownotePayload = {
+  [ShownotePayloadKey.NAME]: '',
+  [ShownotePayloadKey.TIMESTAMP]: 0,
 };
 
-export { DEFAULT_CREATE_EPISODE_PAYLOAD, DEFAULT_CREATE_SHOWNOTE_PAYLOAD };
+export { DEFAULT_CREATE_EPISODE_PAYLOAD, DEFAULT_SHOWNOTE_PAYLOAD };
