238687aff7a499e4400f7cb16767847fb6867c0b
vitalikvv <270vitalik270@gmail.com>
2021-08-10 16:52:26 +0300

    J-104: * fixing after review

--- backend/src/middlewares/check-user-owner/check-user-owner.middleware.ts
+++ backend/src/middlewares/check-user-owner/check-user-owner.middleware.ts
@@ -5,18 +5,14 @@ import { podcast as podcastService } from '~/services/services';
 
 const checkUserOwner = (): RequestHandler => {
   const handler: RequestHandler = async (req, _res, next) => {
-    let podcastId = req.params.id;
-    if (!podcastId) {
-      podcastId = req.body.podcastId;
-    }
+    const podcastId = req.params.id ?? req.body.podcastId;
     const { userId: podcastOwnerId } = await podcastService.getById(podcastId);
 
     if (req.user?.id !== podcastOwnerId) {
-      const err = new HttpError({
+      next(new HttpError({
         status: HttpCode.UNAUTHORIZED,
         message: ErrorMessage.NOT_YOURS_PODCAST,
-      });
-      next(err);
+      }));
     }
 
     next();
