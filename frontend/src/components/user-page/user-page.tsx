7c3e0b1bfd30e5bcc27db37f65d9f725676ebb03
Yevhenii <eucheremisin@gmail.com>
2021-08-10 17:53:57 +0300

    J110: + init user-page

--- frontend/src/components/user-page/user-page.tsx
+++ frontend/src/components/user-page/user-page.tsx
@@ -1,5 +1,9 @@
 import { useAppSelector } from 'hooks/hooks';
 import { RootState } from 'common/types/types';
+import contactLogo from './assets/contact.svg';
+import editLogo from './assets/edit.svg';
+import { PodcastList } from '../homepage/components/components';
+import emailLogo from './assets/email.svg';
 import styles from './styles.module.scss';
 
 const UserPage: React.FC = () => {
@@ -17,7 +21,68 @@ const UserPage: React.FC = () => {
     );
   }
 
-  return <div className={styles.container}></div>;
+  const { podcasts } = useAppSelector(({ homepage }) => ({
+    podcasts: homepage.podcasts,
+  }));
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.userInfo}>
+        <div className={styles.imageContainer}>
+          <img
+            src="http://res.cloudinary.com/hmqu8gtpn/image/upload/v1628597464/2/zxqhnwo2izhud72qbjg6.jpg"
+            width="195"
+            height="195"
+            className={styles.userAvatar}
+          />
+        </div>
+        <div className={styles.mainUserInfo}>
+          <div className={styles.userInfoHeader}>
+            <span className={styles.boldHeaderInfo}>
+              {user?.firstName} {user?.lastName}
+            </span>
+            <span className={styles.headerInfo}>
+              <img
+                src={contactLogo}
+                width="18"
+                height="18"
+                className={styles.headerLogo}
+              />
+              {user?.nickname}
+            </span>
+            <span className={styles.headerInfo}>
+              <img
+                src={emailLogo}
+                width="18"
+                height="18"
+                className={styles.headerLogo}
+              />
+              {user?.email}
+            </span>
+          </div>
+          <div className={styles.bioContainer}>
+            <span className={styles.bioTitle}>Bio:</span>
+            <span className={styles.bioText}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+              quasi sunt vel nulla. Porro, assumenda atque modi quam dolores ab
+              sit vel voluptate unde. Molestias expedita ipsam exercitationem et
+              necessitatibus eos vel a, doloribus qui voluptas dolores eveniet
+            </span>
+          </div>
+          <button className={styles.editButton}>
+            <img src={editLogo} />
+          </button>
+        </div>
+      </main>
+      <div className={styles.line} />
+      <div className={styles.favoritePodcastContainer}>
+        <span className={styles.favoritePodcastTitle}>Favorite Podcasts</span>
+        <div className={styles.podcasts}>
+          <PodcastList podcasts={podcasts} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserPage;
