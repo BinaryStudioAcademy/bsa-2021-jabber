793a66d35690663019032ab37cc7df36da81a010
yaroslav207 <yaroslav.sprynsian8@gmail.com>
2021-08-11 00:38:13 +0300

    J-108: + action user profile

---
+++ frontend/src/store/user-profile/actions.ts
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { Podcast, AsyncThunkConfig, User } from 'common/types/types';
+import { ActionType } from './common';
+
+const loadPodcasts = createAsyncThunk<Podcast[], undefined, AsyncThunkConfig>
+(ActionType.LOAD_PODCASTS, async (_args, { getState, extra }) => {
+  const { podcastApi } = extra;
+  const { auth } = getState();
+  const podcasts = await podcastApi.getPodcastsByUserId((<User>auth.user).id);
+
+  return podcasts;
+});
+
+export { loadPodcasts };
