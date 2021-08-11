53ae372c48c754fead8331f7e25fbf2933558e5f
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-11 14:02:58 +0300

    J-118: + shownotes relationMapping to episode model

--- backend/src/data/models/episode/episode.model.ts
+++ backend/src/data/models/episode/episode.model.ts
@@ -3,6 +3,7 @@ import { TableName, EpisodeDTOKey } from '~/common/enums/enums';
 import { EpisodeType, EpisodeStatus } from '~/common/enums/enums';
 import { Abstract } from '../abstract/abstract.model';
 import { Image } from '~/data/models/image/image.model';
+import { Shownote } from '~/data/models/shownote/shownote.model';
 
 class Episode extends Abstract {
   [EpisodeDTOKey.NAME]: string;
@@ -19,6 +20,8 @@ class Episode extends Abstract {
 
   [EpisodeDTOKey.DESCRIPTION]: string;
 
+  [EpisodeDTOKey.SHOWNOTES]: Shownote[];
+
   [EpisodeDTOKey.STATUS]: EpisodeStatus;
 
   static get tableName(): string {
@@ -32,6 +35,14 @@ class Episode extends Abstract {
       join: {
         from: 'episodes.image_id',
         to: 'images.id',
+      },
+    },
+    shownotes: {
+      relation: Model.HasManyRelation,
+      modelClass: Shownote,
+      join: {
+        from: 'episodes.id',
+        to: 'shownotes.episode_id',
       },
     },
   };
