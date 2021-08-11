d0ab5e3bd71b47309f7c1dd4ffbe64663e21679e
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-11 14:08:05 +0300

    J-118: + shownote-to-form-payload mapper to episode-to-form-payload mapper

--- frontend/src/components/configurate-episode/helpers/map-episode-to-form-payload/map-episode-to-form-payload.ts
+++ frontend/src/components/configurate-episode/helpers/map-episode-to-form-payload/map-episode-to-form-payload.ts
@@ -1,5 +1,6 @@
 import { Episode, EpisodeFormPayload } from 'common/types/types';
 import { EpisodeStatus } from 'common/enums/enums';
+import { mapShownoteToFormPayload } from '../map-shownote-to-form-payload/map-shownote-to-form-payload';
 
 const mapEpisodeToFormPayload = (episode: Episode): EpisodeFormPayload => ({
   name: episode.name,
@@ -8,7 +9,7 @@ const mapEpisodeToFormPayload = (episode: Episode): EpisodeFormPayload => ({
   type: episode.type,
   image: null,
   status: EpisodeStatus.STAGING,
-  shownotes: [],
+  shownotes: episode.shownotes.map(mapShownoteToFormPayload),
 });
 
 export { mapEpisodeToFormPayload };
