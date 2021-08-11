0f26f30381f59fce25bbf5b423ce252e5c41bd7f
Yevhenii <eucheremisin@gmail.com>
2021-08-11 12:27:57 +0300

    J110: * export podcast-item

--- frontend/src/components/common/common.ts
+++ frontend/src/components/common/common.ts
@@ -15,5 +15,5 @@ export { default as Select } from './select/select';
 export { default as Datepiker } from './datepicker/datepicker';
 export { default as Table } from './table/table';
 export { default as PodcastList } from './podcasts-list/podcasts-list';
-export { default as PodcastItem } from './podcast/podcast';
+export { default as PodcastItem } from './podcast-item/podcast-item';
 export { default as Player } from './player/player';
