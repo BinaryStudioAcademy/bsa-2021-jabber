40a7df008137381df6a04c20b70f6b78e2b30eed
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-11 22:44:21 +0300

    J-122: + DefaultImage to PodcastItem

--- frontend/src/components/common/podcast-item/podcast-item.tsx
+++ frontend/src/components/common/podcast-item/podcast-item.tsx
@@ -1,7 +1,6 @@
 import { AppRoute } from 'common/enums/enums';
 import { Podcast as TPodcast } from 'common/types/types';
-import { Link } from 'components/common/common';
-import defaultImage from 'assets/img/default-podcast-image.jpeg';
+import { Link, DefaultImage } from 'components/common/common';
 import styles from './styles.module.scss';
 
 type Props = {
@@ -10,15 +9,22 @@ type Props = {
 
 const Podcast: React.FC<Props> = ({ podcast }) => (
   <li className={styles.wrapper}>
-    <div className={styles.imageWrapper}>
-      <img
-        src={podcast.image?.url ?? defaultImage}
-        className={styles.podcastImage}
-        width="195"
-        height="195"
-        loading="lazy"
+    {podcast.image ? (
+      <div className={styles.imageWrapper}>
+        <img
+          src={podcast.image.url}
+          className={styles.podcastImage}
+          width="195"
+          height="195"
+          loading="lazy"
+        />
+      </div>
+    ) : (
+      <DefaultImage
+        label={podcast.name}
+        className={styles.defaultImageWrapper}
       />
-    </div>
+    )}
     <h3 className={styles.title}>
       <Link to={`${AppRoute.PODCASTS}/${podcast.id}`} className={styles.link}>
         {podcast.name}
