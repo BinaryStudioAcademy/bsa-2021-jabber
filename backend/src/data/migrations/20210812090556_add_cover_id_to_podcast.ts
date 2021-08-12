63cdca595851b1642a2cfc271710efdbd56c9c1c
Yevhenii <eucheremisin@gmail.com>
2021-08-12 12:22:50 +0300

    J133: + new column cover_id to podcasts table

---
+++ backend/src/data/migrations/20210812090556_add_cover_id_to_podcast.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex';
+
+const TABLE_NAME = 'podcasts';
+const COLUMN_NAME = 'cover_id';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.integer(COLUMN_NAME).defaultTo(null);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME);
+  });
+}
