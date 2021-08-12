b91e7b0d87328bd2c056643b835a78b40c6de5ed
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-12 13:06:56 +0300

    J-122: - size constants

--- frontend/src/components/common/default-image/common/constants/constants.ts
+++ frontend/src/components/common/default-image/common/constants/constants.ts
@@ -1,34 +1,4 @@
 const SYMBOLS_COUNT_IN_LABEL = 1;
 const DEFAULT_LABEL = 'Undefined';
 
-const SMALL_DEFAULT_IMAGE: React.CSSProperties = {
-  width: '40px',
-  height: '40px',
-  fontSize: '14px',
-  borderWidth: '2px',
-  borderRadius: '50%',
-};
-
-const MEDIUM_DEFAULT_IMAGE: React.CSSProperties = {
-  width: '195px',
-  height: '195px',
-  fontSize: '77px',
-  borderWidth: '11px',
-  borderRadius: '3px',
-};
-
-const LARGE_DEFAULT_IMAGE: React.CSSProperties = {
-  width: '280px',
-  height: '280px',
-  fontSize: '110px',
-  borderWidth: '13px',
-  borderRadius: '5px',
-};
-
-export {
-  SMALL_DEFAULT_IMAGE,
-  MEDIUM_DEFAULT_IMAGE,
-  LARGE_DEFAULT_IMAGE,
-  SYMBOLS_COUNT_IN_LABEL,
-  DEFAULT_LABEL,
-};
+export { SYMBOLS_COUNT_IN_LABEL, DEFAULT_LABEL };
