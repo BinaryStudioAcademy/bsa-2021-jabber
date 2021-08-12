473cff88799672e2d02123e4adc9c6b1dbe06bf4
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-12 13:07:30 +0300

    J-122: - get default image style helper

--- frontend/src/components/common/default-image/common/helpers/helpers.ts
+++ frontend/src/components/common/default-image/common/helpers/helpers.ts
@@ -1,2 +1,1 @@
 export { getLabelSubstring } from './get-label-substring/get-label-substring.helper';
-export { getDefaultImageStyle } from './get-default-image-style/get-default-image-style.helper';
