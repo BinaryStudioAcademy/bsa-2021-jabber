0985ce9c1893d0a71e8d5fdbe60da68a0a6bd1b6
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-12 13:09:12 +0300

    J-122: - DefaultImageSize & getDefaultImageStyle helper from DefaultImage

--- frontend/src/components/common/default-image/default-image.tsx
+++ frontend/src/components/common/default-image/default-image.tsx
@@ -1,28 +1,19 @@
-import { DefaultImageSize } from 'common/enums/ui/ui';
 import { getAllowedClasses } from 'helpers/dom/dom';
 import { DEFAULT_LABEL } from './common/constants/constants';
-import {
-  getDefaultImageStyle,
-  getLabelSubstring,
-} from './common/helpers/helpers';
+import { getLabelSubstring } from './common/helpers/helpers';
 import styles from './styles.module.scss';
 
 type Props = {
-  size?: DefaultImageSize;
   label?: string;
   className?: string;
 };
 
 const DefaultImage: React.FC<Props> = ({
-  size = DefaultImageSize.MEDIUM,
   label = DEFAULT_LABEL,
   className,
 }) => {
   return (
-    <div
-      style={getDefaultImageStyle(size)}
-      className={getAllowedClasses(styles.defaultImage, className)}
-    >
+    <div className={getAllowedClasses(styles.defaultImage, className)}>
       <span>{getLabelSubstring(label)}</span>
     </div>
   );
