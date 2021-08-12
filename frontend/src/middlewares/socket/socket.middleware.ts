a842dd6964f0600efef2bd5e959aec47765dbe07
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-08-12 18:49:55 +0300

    J-92: * types

--- frontend/src/middlewares/socket/socket.middleware.ts
+++ frontend/src/middlewares/socket/socket.middleware.ts
@@ -1,18 +1,20 @@
-import { Middleware } from '@reduxjs/toolkit';
+import { Middleware, AnyAction } from '@reduxjs/toolkit';
 import { io } from 'socket.io-client';
 import { ENV, SocketEvent, DataStatus } from 'common/enums/enums';
 import { Comment } from 'common/types/types';
 import { ActionType as EpisodeActionType } from 'store/episode/common';
 import { episode as episodeAction } from 'store/actions';
 
+type Next = (action: AnyAction) => void;
+
 const socket = io(ENV.SOCKET_SERVER);
 
-const socketMiddleware: Middleware = ({ dispatch }) => (next): any => {
+const socketMiddleware: Middleware = ({ dispatch }) => (next): Next => {
   socket.on(SocketEvent.UPDATE_COMMENTS, (comment: Comment): void => {
     dispatch(episodeAction.updateComments(comment));
   });
 
-  return (action: any): void => {
+  return (action: AnyAction): void => {
     switch (action.type) {
       case `${EpisodeActionType.LOAD_COMMENTS_BY_EPISODE_ID}/${DataStatus.PENDING}`: {
         socket.emit(SocketEvent.JOIN_ROOM, action.meta.arg);
