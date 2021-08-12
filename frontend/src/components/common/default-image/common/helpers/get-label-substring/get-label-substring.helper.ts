c15a2d30c616faa7881ee8043a0ce940ab402c5c
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-12 19:30:45 +0300

    J-122: - DefaultImage component

--- frontend/src/components/common/default-image/common/helpers/get-label-substring/get-label-substring.helper.ts
+++
@@ -1,8 +0,0 @@
-import { FIRST_ARRAY_IDX } from 'common/constants/constants';
-import { SYMBOLS_COUNT_IN_LABEL } from '../../constants/constants';
-
-const getLabelSubstring = (label: string): string => {
-  return label.substring(FIRST_ARRAY_IDX, SYMBOLS_COUNT_IN_LABEL).toUpperCase();
-};
-
-export { getLabelSubstring };
