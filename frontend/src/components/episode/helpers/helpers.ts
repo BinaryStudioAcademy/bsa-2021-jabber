42ad69aad58415eb49e068fba288ea7e19652338
vitalik-prog <270vitalik270@gmail.com>
2021-08-12 15:21:42 +0300

    J-117: * delete redundant code

--- frontend/src/components/episode/helpers/helpers.ts
+++ frontend/src/components/episode/helpers/helpers.ts
@@ -1,2 +1,1 @@
 export { getCurrentTime } from './get-current-time/get-current-time.helper';
-export { getTimeFormatHHMMSS } from './shownote-time/shownote-time';
--- frontend/src/components/episode/helpers/shownote-time/shownote-time.ts
+++
@@ -1,7 +0,0 @@
-import { MILLISECONDS_IN_SECOND } from '../../../common/player/common/constants';
-
-const getTimeFormatHHMMSS = (seconds: number): string => {
-  return new Date(seconds * MILLISECONDS_IN_SECOND).toISOString().substr(11, 8);
-};
-
-export { getTimeFormatHHMMSS };
