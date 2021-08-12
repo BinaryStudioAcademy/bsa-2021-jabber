4757a7f05f208c813304cb60e62fb65603732fe9
Artem-Vasylevytskyi <artem.totality@gmail.com>
2021-08-12 23:46:43 +0300

    J-131: + delete & deleteByEpisodeId to record service

--- backend/src/services/record/record.service.ts
+++ backend/src/services/record/record.service.ts
@@ -19,6 +19,18 @@ class Record {
   public create(payload: RecordCreatePayload): Promise<TRecord> {
     return this.#recordRepository.create(payload);
   }
+
+  public delete(id: number): Promise<TRecord> {
+    return this.#recordRepository.delete(id);
+  }
+
+  public async deleteByEpisodeId(id: number): Promise<void> {
+    const record = await this.#recordRepository.getByEpisodeId(id);
+
+    if (record) {
+      await this.delete(record.id);
+    }
+  }
 }
 
 export { Record };
