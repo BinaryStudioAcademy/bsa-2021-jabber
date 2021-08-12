890cf4904f071129deacd1b4f46c201e6031b26f
JuliaDzyuba <60258687+JuliaDzyuba@users.noreply.github.com>
2021-08-12 20:06:21 +0300

    J-137: changed name of types

--- frontend/src/components/podcast/components/episode-table/helpers/get-columns.helper.tsx
+++ frontend/src/components/podcast/components/episode-table/helpers/get-columns.helper.tsx
@@ -2,11 +2,12 @@ import { Column } from 'react-table';
 import { INCREASE_CONT_FOR_IDX } from 'common/constants/constants';
 import { Link } from 'components/common/common';
 import { AppRoute } from 'common/enums/enums';
+import { EpisodeNameRow } from '../types/types';
 
-type Row = {
-  name: string,
-  episodeId: string,
-};
+// type EpisodeNameRow = {
+//   name: string,
+//   episodeId: string,
+// };
 
 const getColumns = (): Column[] => {
   const columns: Column[] = [
@@ -17,15 +18,15 @@ const getColumns = (): Column[] => {
       },
     },
     {
-      Header: 'Name and Host',
+      Header: 'Name',
       accessor: (originalRow): Record<string, unknown> => {
-        const row = originalRow as Row;
+        const row = originalRow as EpisodeNameRow;
         return {
           name: row.name,
           episodeId: row.episodeId,
         };
       },
-      Cell: function getLink({ value }): JSX.Element  {
+      Cell: function getLink({ value }): JSX.Element  { 
         return (
           <Link to={`${AppRoute.EPISODES}/${value.episodeId}`} >{value.name}</Link>
         );
