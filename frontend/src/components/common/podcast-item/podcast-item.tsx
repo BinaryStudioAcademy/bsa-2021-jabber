f00c0dc8673cba02907f59e4792eeb85cb3683b6
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-12 19:45:49 +0300

    Revert "J-122: + ImageWrapper to PodcastItem"
    This reverts commit 70dd5799ead01c515ebed8723d86c51849d1f590.

--- frontend/src/components/common/podcast-item/podcast-item.tsx
+++ frontend/src/components/common/podcast-item/podcast-item.tsx
@@ -1,6 +1,6 @@
 import { AppRoute } from 'common/enums/enums';
 import { Podcast as TPodcast } from 'common/types/types';
-import { Link, ImageWrapper } from 'components/common/common';
+import { Link, DefaultImage } from 'components/common/common';
 import styles from './styles.module.scss';
 
 type Props = {
@@ -9,15 +9,19 @@ type Props = {
 
 const Podcast: React.FC<Props> = ({ podcast }) => (
   <li className={styles.wrapper}>
-    <ImageWrapper
-      src={podcast.image?.url}
-      width="195"
-      height="195"
-      loading="lazy"
-      alt={podcast.name}
-      label={podcast.name}
-      className={styles.imageWrapper}
-    />
+    <div className={styles.imageWrapper}>
+      {podcast.image ? (
+        <img
+          src={podcast.image.url}
+          className={styles.podcastImage}
+          width="195"
+          height="195"
+          loading="lazy"
+        />
+      ) : (
+        <DefaultImage label={podcast.name} />
+      )}
+    </div>
     <h3 className={styles.title}>
       <Link to={`${AppRoute.PODCASTS}/${podcast.id}`} className={styles.link}>
         {podcast.name}
