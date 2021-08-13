9bd260ff2dc4ca67b0d65e1f2fa9e0939398da08
yaroslav207 <75075327+yaroslav207@users.noreply.github.com>
2021-08-13 19:33:19 +0300

    J-134: + migration add genre_id to podcasts

---
+++ backend/src/data/migrations/20210813151449_add_genre_id_to_podcasts.ts
@@ -0,0 +1,20 @@
+import { Knex } from "knex";
+
+const TABLE_NAME = 'podcasts';
+const COLUMN_NAME = 'genre_id';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table
+      .integer(COLUMN_NAME)
+      .references('id')
+      .inTable('genres')
+      .notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME);
+  });
+}
