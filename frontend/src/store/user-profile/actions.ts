a32f6133fcc18544ad6e04093dbd6aeea4fdccc3
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-14 18:41:12 +0300

    J-143: + update user action to redux

--- frontend/src/store/user-profile/actions.ts
+++ frontend/src/store/user-profile/actions.ts
@@ -1,21 +1,48 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { Podcast, AsyncThunkConfig, User } from 'common/types/types';
+import { NotificationMessage, NotificationTitle } from 'common/enums/enums';
+import {
+  Podcast,
+  AsyncThunkConfig,
+  User,
+  UserEditPayload,
+} from 'common/types/types';
 import { ActionType } from './common';
 
-const loadPodcasts = createAsyncThunk<Podcast[], number, AsyncThunkConfig>
-(ActionType.LOAD_PODCASTS, async (id, { extra }) => {
-  const { podcastApi } = extra;
-  const podcasts = await podcastApi.getAllByUserId(id);
+const loadPodcasts = createAsyncThunk<Podcast[], number, AsyncThunkConfig>(
+  ActionType.LOAD_PODCASTS,
+  async (id, { extra }) => {
+    const { podcastApi } = extra;
+    const podcasts = await podcastApi.getAllByUserId(id);
 
-  return podcasts;
-});
+    return podcasts;
+  },
+);
 
-const loadUser = createAsyncThunk<User, number, AsyncThunkConfig>
-(ActionType.LOAD_USER, async (id, { extra }) => {
-  const { userApi } = extra;
-  const user = await userApi.getById(id);
+const loadUser = createAsyncThunk<User, number, AsyncThunkConfig>(
+  ActionType.LOAD_USER,
+  async (id, { extra }) => {
+    const { userApi } = extra;
+    const user = await userApi.getById(id);
 
-  return user;
-});
+    return user;
+  },
+);
 
-export { loadPodcasts, loadUser };
+const updateUser = createAsyncThunk<User, UserEditPayload, AsyncThunkConfig>(
+  ActionType.UPDATE_USER,
+  async (payload, { extra, getState }) => {
+    const { userApi, notificationService } = extra;
+    const { auth } = getState();
+
+    const updatedUser = await userApi.update((<User>auth.user).id, payload);
+
+    notificationService.success(
+      NotificationTitle.SUCCESS,
+      NotificationMessage.USER_UPDATED,
+    );
+
+    return updatedUser;
+  },
+);
+
+export { loadPodcasts, loadUser, updateUser };
--- frontend/src/store/user-profile/common.ts
+++ frontend/src/store/user-profile/common.ts
@@ -1,6 +1,7 @@
 enum ActionType {
   LOAD_PODCASTS = 'user-profile/load-podcasts',
   LOAD_USER = 'user-profile/load-user',
+  UPDATE_USER = 'user-profile/update-user',
 }
 
 export { ActionType };
