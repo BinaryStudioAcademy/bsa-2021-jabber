1beecf02b68e15cece666a89bdf8f4ce5a167af9
Artem-Vasylevytskyi <artem.totality@gmail.com>
2021-08-14 10:45:12 +0300

    J-131: * fix-issues

--- frontend/src/components/configurate-episode/configurate-episode.tsx
+++ frontend/src/components/configurate-episode/configurate-episode.tsx
@@ -1,9 +1,9 @@
 import { useDispatch, useParams, useEffect, useAppSelector } from 'hooks/hooks';
 import { configurateEpisode as configurateEpisodeActions } from 'store/actions';
 import { EpisodeFormPayload } from 'common/types/types';
-import { DataStatus, ButtonType, ButtonColor } from 'common/enums/enums';
+import { DataStatus } from 'common/enums/enums';
 import { mapEpisodeToFormPayload } from './helpers/helpers';
-import { Loader, Button } from 'components/common/common';
+import { Loader } from 'components/common/common';
 import { CreateEpisodeForm } from './components/components';
 import { PageParams } from './common/types/types';
 import styles from './styles.module.scss';
@@ -12,16 +12,14 @@ const ConfigurateEpisode: React.FC = () => {
   const { id, podcastId } = useParams<PageParams>();
   const dispatch = useDispatch();
 
-  const { episode, dataStatus, user } = useAppSelector(({ configurateEpisode, auth }) => ({
+  const { episode, dataStatus } = useAppSelector(({ configurateEpisode }) => ({
     episode: configurateEpisode.episode,
     dataStatus: configurateEpisode.dataStatus,
-    user: auth.user,
   }));
 
   const mapEpisode = episode ? mapEpisodeToFormPayload(episode) : undefined;
 
   const isEdit = Boolean(id);
-  const isEpisodeOwner = user?.id === episode?.userId; 
 
   const isLoading = dataStatus === DataStatus.PENDING;
 
@@ -32,13 +30,6 @@ const ConfigurateEpisode: React.FC = () => {
         ...payload,
         podcastId: Number(podcastId),
       }));
-  };
-
-  const handleDeleteEpisode = (): void => {
-    dispatch(configurateEpisodeActions.deleteEpisode({
-      episodeId: Number(id),
-      podcastId: Number(podcastId),
-    }));
   };
 
   useEffect(() => {
@@ -52,21 +43,12 @@ const ConfigurateEpisode: React.FC = () => {
       <h2>
         {isEdit ? 'Edit' : 'Create'} Episode {episode?.name ?? ''}
       </h2>
-      {isLoading ? (
-        <Loader />
-      ) : isEdit ? isEpisodeOwner ? (
-        <>
-          <CreateEpisodeForm onSubmit={handleFormSubmit} payload={mapEpisode} />
-          <Button
-            label="Delete"
-            type={ButtonType.BUTTON}
-            buttonColor={ButtonColor.LIGHT_NAVY}
-            onClick={handleDeleteEpisode}
-          />
-        </>
-      ) : 
-        <div>Episode is not available!</div> 
-        : <CreateEpisodeForm onSubmit={handleFormSubmit} payload={mapEpisode} />}
+      {isLoading
+        ? <Loader />
+        : <CreateEpisodeForm
+          onSubmit={handleFormSubmit}
+          payload={mapEpisode} />
+      }
     </div>
   );
 };
