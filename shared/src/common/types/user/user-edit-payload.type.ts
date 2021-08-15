4c0b4bf394a7a2fc4187e9235a43f1509df4bb4d
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-15 17:02:48 +0300

    J-143: - 'password' field from 'user edit' payload & validation schema

--- shared/src/common/types/user/user-edit-payload.type.ts
+++ shared/src/common/types/user/user-edit-payload.type.ts
@@ -2,7 +2,6 @@ import { UserPayloadKey } from '~/common/enums/enums';
 import { UserConfigurePayload } from './user-configure-payload.type';
 
 type UserEditPayload = UserConfigurePayload & {
-  [UserPayloadKey.PASSWORD]?: string;
   [UserPayloadKey.BIO]: string;
 };
 
--- shared/src/validation-schemas/user/user-edit/user-edit.validation-schema.ts
+++ shared/src/validation-schemas/user/user-edit/user-edit.validation-schema.ts
@@ -7,15 +7,6 @@ import {
 import { configurateUser } from '../configurate-user/configurate-user.validation-schema';
 
 const userEdit = configurateUser.keys({
-  [UserPayloadKey.PASSWORD]: Joi.string()
-    .trim()
-    .min(UserValidationRule.PASSWORD_MIN_LENGTH)
-    .max(UserValidationRule.PASSWORD_MAX_LENGTH)
-    .messages({
-      'string.min': UserValidationMessage.PASSWORD_MIN_LENGTH,
-      'string.max': UserValidationMessage.PASSWORD_MAX_LENGTH,
-    })
-    .allow(null, ''),
   [UserPayloadKey.BIO]: Joi.string()
     .trim()
     .max(UserValidationRule.BIO_MAX_LENGTH)
