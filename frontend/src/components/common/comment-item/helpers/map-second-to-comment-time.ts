9a91b56562d372087c7a932c150e9e7671b48437
yaroslav207 <75075327+yaroslav207@users.noreply.github.com>
2021-08-16 11:30:26 +0300

    J-153: fix healper map seconds to comment time

--- frontend/src/components/common/comment-item/helpers/map-second-to-comment-time.ts
+++ frontend/src/components/common/comment-item/helpers/map-second-to-comment-time.ts
@@ -1,8 +1,21 @@
-import { addSeconds, format } from 'date-fns';
+import { intervalToDuration } from 'date-fns';
 
-const mapSecondToCommentTime = (seconds: number): string => {
-  const helperDate = addSeconds(new Date(0), seconds);
-  return format(helperDate, 'mm:ss');
+const mapSecondToCommentTime = (intervalInSeconds: number): string => {
+  const normalizeTime = (timeNumber: number | undefined): string => {
+    if (timeNumber) {
+      const time = timeNumber.toString();
+      return time.length === 1 ? `0${time}` : time;
+    }
+    return '00';
+  };
+
+  const helperDate = intervalToDuration({ start: 0, end: intervalInSeconds * 1000 });
+
+  const hours = normalizeTime(helperDate.hours);
+  const minutes = normalizeTime(helperDate.minutes);
+  const seconds =  normalizeTime(helperDate.seconds);
+
+  return `${hours}:${minutes}:${seconds}`;
 };
 
 export { mapSecondToCommentTime };
