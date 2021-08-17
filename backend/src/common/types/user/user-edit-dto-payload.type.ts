783aca477584e06955f4e1a4a587dfa573a20129
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-08-17 16:26:28 +0300

    J-165: + types

--- backend/src/common/types/user/user-edit-dto-payload.type.ts
+++ backend/src/common/types/user/user-edit-dto-payload.type.ts
@@ -1,14 +1,13 @@
 import { UserPayloadKey } from '~/common/enums/enums';
-import { UserPayload } from './user-payload.type';
 
-type UserEditPayload = UserPayload & {
+type UserEditDTOPayload = {
   [UserPayloadKey.FIRST_NAME]: string;
   [UserPayloadKey.LAST_NAME]: string;
   [UserPayloadKey.NICKNAME]: string;
+  [UserPayloadKey.EMAIL]: string;
   [UserPayloadKey.BIRTHDATE]: Date;
   [UserPayloadKey.BIO]: string;
-  [UserPayloadKey.IMAGE_DATA_URL]: string | null;
   [UserPayloadKey.IMAGE_ID]: number | null;
 };
 
-export type { UserEditPayload };
+export type { UserEditDTOPayload };
