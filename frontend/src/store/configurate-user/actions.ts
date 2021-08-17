ad3626829138557e9d3dbc2a3805f574e603cf77
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-08-17 16:30:56 +0300

    J-165: * update-edit-form

--- frontend/src/store/configurate-user/actions.ts
+++ frontend/src/store/configurate-user/actions.ts
@@ -4,16 +4,28 @@ import {
   NotificationMessage,
   NotificationTitle,
 } from 'common/enums/enums';
-import { AsyncThunkConfig, User, UserEditPayload } from 'common/types/types';
+import { AsyncThunkConfig, User, UserEditFormPayload } from 'common/types/types';
 import { ActionType } from './common';
+import { getDataUrl, getFileFromFileList } from 'helpers/helpers';
 
-const editUser = createAsyncThunk<User, UserEditPayload, AsyncThunkConfig>(
+const editUser = createAsyncThunk<User, UserEditFormPayload, AsyncThunkConfig>(
   ActionType.EDIT_USER,
   async (payload, { extra, getState }) => {
     const { userApi, notificationService, navigationService } = extra;
     const { auth } = getState();
+    const { id, imageId } = <User>auth.user;
+    const file = getFileFromFileList(payload.image);
 
-    const user = await userApi.update((<User>auth.user).id, payload);
+    const user = await userApi.update(id, {
+      firstName: payload.firstName,
+      lastName: payload.lastName,
+      nickname: payload.nickname,
+      bio: payload.bio,
+      email: payload.email,
+      birthdate: payload.birthdate,
+      imageDataUrl: file ? await getDataUrl(file) : null,
+      imageId,
+    });
 
     notificationService.success(
       NotificationTitle.SUCCESS,
