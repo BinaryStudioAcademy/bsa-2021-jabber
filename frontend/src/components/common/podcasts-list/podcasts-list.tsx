6b8a16e67e222480b13565991adc92cad93ad773
Yevhenii <eucheremisin@gmail.com>
2021-08-17 14:22:21 +0300

    J55: + loader to searched podcasts

--- frontend/src/components/common/podcasts-list/podcasts-list.tsx
+++ frontend/src/components/common/podcasts-list/podcasts-list.tsx
@@ -1,17 +1,29 @@
 import { Podcast as TPodcast } from 'common/types/types';
-import { PodcastItem } from 'components/common/common';
+import { PodcastItem, Loader } from 'components/common/common';
+import { DataStatus } from 'common/enums/enums';
+import { useAppSelector } from 'hooks/hooks';
 import styles from './styles.module.scss';
 
 type Props = {
   podcasts: TPodcast[];
 };
 
-const PodcastsList: React.FC<Props> = ({ podcasts }) => (
-  <ul className={styles.list}>
-    {podcasts.map((it) => (
-      <PodcastItem podcast={it} key={it.id} />
-    ))}
-  </ul>
-);
+const PodcastsList: React.FC<Props> = ({ podcasts }) => {
+  const { dataStatus } = useAppSelector(({ homepage }) => ({
+    dataStatus: homepage.dataStatus,
+  }));
+
+  if (dataStatus === DataStatus.PENDING) {
+    return <Loader />;
+  }
+
+  return (
+    <ul className={styles.list}>
+      {podcasts.map((it) => {
+        return <PodcastItem podcast={it} key={it.id} />;
+      })}
+    </ul>
+  );
+};
 
 export default PodcastsList;
