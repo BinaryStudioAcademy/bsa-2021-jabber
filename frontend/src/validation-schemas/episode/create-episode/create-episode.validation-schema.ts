02ccb16c0ba225cfe07c2fda5ef755ab4ac50a80
Andrii Solohub <andrei.sologub12@gmail.com>
2021-08-17 12:59:43 +0300

    J-173: * validation for episode record & image

--- frontend/src/validation-schemas/episode/create-episode/create-episode.validation-schema.ts
+++ frontend/src/validation-schemas/episode/create-episode/create-episode.validation-schema.ts
@@ -1,10 +1,36 @@
 import { Joi } from 'helpers/helpers';
-import { EpisodePayloadKey } from 'common/enums/enums';
+import { EpisodePayloadKey, FileExtension } from 'common/enums/enums';
 import { episode } from 'jabber-shared/validation-schemas/validation-schemas';
+import { fileExtensionValidation } from './shared/helpers/helpers';
+import { EpisodeValidationMessage } from 'jabber-shared/common/enums/enums';
 
 const episodeCreate = episode.keys({
-  [EpisodePayloadKey.IMAGE]: Joi.object().allow(null),
-  [EpisodePayloadKey.RECORD]: Joi.object().allow(null),
+  [EpisodePayloadKey.IMAGE]: Joi.object()
+    .custom(
+      fileExtensionValidation(
+        FileExtension.JPEG,
+        FileExtension.JPG,
+        FileExtension.PNG,
+        FileExtension.SVG,
+      ),
+      'file extension validation',
+    )
+    .allow(null)
+    .messages({
+      'file.invalidExtension': EpisodeValidationMessage.DATA_URL_FORMAT_IMG,
+    }),
+  [EpisodePayloadKey.RECORD]: Joi.object()
+    .custom(
+      fileExtensionValidation(
+        FileExtension.MP3,
+        FileExtension.WAV,
+      ),
+      'file extension validation',
+    )
+    .allow(null)
+    .messages({
+      'file.invalidExtension': EpisodeValidationMessage.DATA_URL_FORMAT,
+    }),
 });
 
 export { episodeCreate };
