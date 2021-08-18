2e778e3b34496f2a48a20ca9af0c0c7eff2f2baf
Andrii Solohub <andrei.sologub12@gmail.com>
2021-08-18 12:16:58 +0300

    J-157: fixed migrate on down

--- backend/src/data/migrations/20210817163051_update_episode_status_enum.ts
+++ backend/src/data/migrations/20210817163051_update_episode_status_enum.ts
@@ -13,13 +13,12 @@ const formatAlterTableEnumSql = (
   TABLE_NAME: string,
   COLUMN_NAME: string,
   EpisodeStatus: string[],
-) => {
+): string => {
   const constraintName = `${TABLE_NAME}_${COLUMN_NAME}_check`;
   return [
     `ALTER TABLE ${TABLE_NAME} DROP CONSTRAINT IF EXISTS ${constraintName};`,
     `ALTER TABLE ${TABLE_NAME} ADD CONSTRAINT ${constraintName} CHECK (${COLUMN_NAME} = ANY (ARRAY['${EpisodeStatus.join(
-      "'::text, '"
-    )}'::text]));`,
+      '\'::text, \'')}'::text]));`,
   ].join('\n');
 };
 
@@ -29,9 +28,8 @@ export async function up(knex: Knex): Promise<void> {
       EpisodeStatus.PUBLISHED,
       EpisodeStatus.STAGING,
       EpisodeStatus.LIVE,
-    ])
+    ]),
   );
-
 }
 
 export async function down(knex: Knex): Promise<void> {
@@ -39,6 +37,7 @@ export async function down(knex: Knex): Promise<void> {
     formatAlterTableEnumSql(TABLE_NAME, COLUMN_NAME, [
       EpisodeStatus.PUBLISHED,
       EpisodeStatus.STAGING,
-    ])
+      EpisodeStatus.LIVE,
+    ]),
   );
 }
