fec1b473630a18664f010b6f17ab6b63722bea9d
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-19 20:41:29 +0300

    J-185: * made shared validation more common

--- shared/src/validation-schemas/shownote/shownote.validation-schema.ts
+++ shared/src/validation-schemas/shownote/shownote.validation-schema.ts
@@ -16,16 +16,6 @@ const shownote = Joi.object({
       'string.min': ShownoteValidationMessage.SHOWNOTE_NAME_MIN_LENGTH,
       'string.max': ShownoteValidationMessage.SHOWNOTE_NAME_MAX_LENGTH,
     }),
-  [ShownotePayloadKey.TIMESTAMP]: Joi.number()
-    .integer()
-    .min(ShownoteValidationRule.TIMESTAMP_MIN_VALUE)
-    .required()
-    .messages({
-      'number.required': ShownoteValidationMessage.TIMESTAMP_REQUIRE,
-      'number.base': ShownoteValidationMessage.TIMESTAMP_BASE,
-      'number.integer': ShownoteValidationMessage.TIMESTAMP_NUMBER_FORMAT,
-      'number.min': ShownoteValidationMessage.TIMESTAMP_MIN_VALUE,
-    }),
 });
 
 export { shownote };
