c7f4b2cdcaf49ca7b47cf6641ed9980cd49bde86
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-08-20 14:09:09 +0300

    J-192: + socket-event-enums

--- shared/src/common/enums/socket/socket-event.enum.ts
+++ shared/src/common/enums/socket/socket-event.enum.ts
@@ -1,8 +1,15 @@
 enum SocketEvent {
   CONNECTION = 'connection',
+  DISCONNECT = 'disconnect',
   JOIN_ROOM = 'joinRoom',
   LEAVE_ROOM = 'leaveRoom',
   UPDATE_COMMENTS = 'updateComments',
+  PEER_BROADCASTER = 'peerBroadcaster',
+  PEER_WATCHER = 'peerWatcher',
+  PEER_DISCONNECT = 'peerDisconnect',
+  PEER_OFFER = 'peerOffer',
+  PEER_ANSWER = 'peerAnswer',
+  PEER_CANDIDATE = 'peerCandidate',
 }
 
 export { SocketEvent };
