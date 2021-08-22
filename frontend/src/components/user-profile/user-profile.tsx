401d5ad40094e624905382608620b05dd3b87b9b
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-08-22 10:55:09 +0300

    J-228: + follow-button

--- frontend/src/components/user-profile/user-profile.tsx
+++ frontend/src/components/user-profile/user-profile.tsx
@@ -3,9 +3,10 @@ import {
   PodcastList,
   ImageWrapper,
   Link,
+  Button,
 } from 'components/common/common';
-import { AppRoute, DataStatus } from 'common/enums/enums';
-import { RootState } from 'common/types/types';
+import { AppRoute, DataStatus, ButtonColor } from 'common/enums/enums';
+import { RootState, UserFollowerPayload } from 'common/types/types';
 import { useAppSelector, useParams, useDispatch, useEffect } from 'hooks/hooks';
 import { userProfile as userProfileActions } from 'store/actions';
 import contactLogo from 'assets/img/user-profile/contact.svg';
@@ -16,12 +17,13 @@ import styles from './styles.module.scss';
 const UserPage: React.FC = () => {
   const { id } = useParams<PageParams>();
 
-  const { currentUser, user, podcasts, dataStatus } = useAppSelector(
+  const { currentUser, user, podcasts, dataStatus, isFollowed } = useAppSelector(
     ({ auth, userProfile }: RootState) => ({
       currentUser: auth.user,
       user: userProfile.user,
       podcasts: userProfile.podcasts,
       dataStatus: userProfile.dataStatus,
+      isFollowed: userProfile.isFollowed,
     }),
   );
 
@@ -32,8 +34,30 @@ const UserPage: React.FC = () => {
     dispatch(userProfileActions.loadPodcasts(Number(id)));
   }, [id]);
 
+  useEffect(() => {
+    if (user && currentUser) {
+      dispatch(userProfileActions.isFollowedUser({
+        userId: user.id,
+        followerId: currentUser.id,
+      }));
+    }
+  }, [user, currentUser]);
+
   const hasUser = Boolean(user);
   const hasPermitToEdit = currentUser?.id === Number(id);
+
+  const handleFollow = (): void => {
+    if (user && currentUser) {
+      const payload: UserFollowerPayload = {
+        userId: user.id,
+        followerId: currentUser.id,
+      };
+
+      isFollowed
+        ? dispatch(userProfileActions.unfollowUser(payload))
+        : dispatch(userProfileActions.followUser(payload));
+    }
+  };
 
   if (!hasUser && dataStatus === DataStatus.FULFILLED) {
     return (
@@ -96,6 +120,16 @@ const UserPage: React.FC = () => {
           />
         )}
       </main>
+      <div className={styles.followContainer}>
+        <h2 className={styles.followTitle}>Followers:</h2>
+        <span className={styles.followCount}>{user?.followersCount}</span>
+        <Button
+          className={styles.followButton}
+          label={isFollowed ? 'Unfollow' : 'Follow'}
+          buttonColor={isFollowed ? ButtonColor.LIGHT_PINK : ButtonColor.PINK}
+          onClick={handleFollow}
+        />
+      </div>
       <div className={styles.podcastsByUserContainer}>
         <h2 className={styles.podcastsByUserTitle}>My Podcasts</h2>
         {podcasts.length ? (
