514cf094afb93cb2569ca7c99e0af86906a9b0c6
JuliaDzyuba <60258687+JuliaDzyuba@users.noreply.github.com>
2021-08-23 04:51:09 +0300

    J-222: * popuper component

--- frontend/src/components/common/popuper/popuper.tsx
+++ frontend/src/components/common/popuper/popuper.tsx
@@ -1,1 +1,27 @@
-export { default as Popup } from 'reactjs-popup';
+import Popup from 'reactjs-popup';
+// import styles from './styles.module.scss';
+
+type Props = {
+  trigger: JSX.Element,
+};
+
+const PopUp: React.FC<Props> = ({
+  trigger,
+  children,
+}) => {
+  return (
+    <Popup
+      trigger={trigger}
+      on="click"
+      closeOnDocumentClick
+      mouseLeaveDelay={300}
+      mouseEnterDelay={0}
+      arrow={false}
+      // {...styles}
+    >
+      {children}
+    </Popup>
+  );
+};
+
+export default PopUp;
