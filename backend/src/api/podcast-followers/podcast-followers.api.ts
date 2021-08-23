7ff8b145c73250de96f760f5893f1165f353e947
yaroslav207 <yaroslav.sprynsian8@gmail.com>
2021-08-23 12:36:56 +0300

    J-229: + route for podcast follover

---
+++ backend/src/api/podcast-followers/podcast-followers.api.ts
@@ -0,0 +1,67 @@
+import { Router } from 'express';
+import { ApiPath, HttpCode, PodcastsFollowersApiPath, HttpMethod } from '~/common/enums/enums';
+import { podcastFollower as podcastFollowerService } from '~/services/services';
+import { handleAsyncApi } from '~/helpers/helpers';
+import {
+  checkAuth as checkAuthMiddleware,
+  validateSchema as validateSchemaMiddleware,
+} from '~/middlewares/middlewares';
+import { podcastFollower as podcastFollowerValidationSchema } from '~/validation-schemas/validation-schemas';
+
+type Args = {
+  apiRouter: Router;
+  podcastFollowerService: typeof podcastFollowerService;
+};
+
+const initPodcastFollowersApi = ({ apiRouter, podcastFollowerService }: Args): Router => {
+  const userFollowerRouter = Router();
+
+  apiRouter.use(ApiPath.PODCAST_FOLLOWERS, userFollowerRouter);
+
+  userFollowerRouter.get(
+    PodcastsFollowersApiPath.$ID,
+    handleAsyncApi(async (req, res) => {
+      return res
+        .json(await podcastFollowerService.getCountByPodcastId(Number(req.params.id)))
+        .status(HttpCode.OK);
+    }),
+  );
+
+  userFollowerRouter.get(
+    PodcastsFollowersApiPath.$PODCAST_ID_$FOLLOWER_ID,
+    checkAuthMiddleware(HttpMethod.GET),
+    handleAsyncApi(async (req, res) => {
+      const isFollowed = await podcastFollowerService.checkIsFollowed({
+        podcastId: Number(req.params.podcastId),
+        followerId: Number(req.params.followerId),
+      });
+
+      return res.json(isFollowed).status(HttpCode.OK);
+    }),
+  );
+
+  userFollowerRouter.post(
+    PodcastsFollowersApiPath.ROOT,
+    checkAuthMiddleware(HttpMethod.POST),
+    validateSchemaMiddleware(podcastFollowerValidationSchema),
+    handleAsyncApi(async (req, res) => {
+      return res
+        .json(await podcastFollowerService.create(req.body))
+        .status(HttpCode.CREATED);
+    }),
+  );
+
+  userFollowerRouter.delete(
+    PodcastsFollowersApiPath.ROOT,
+    checkAuthMiddleware(HttpMethod.DELETE),
+    handleAsyncApi(async (req, res) => {
+      return res
+        .json(await podcastFollowerService.delete(req.body))
+        .status(HttpCode.OK);
+    }),
+  );
+
+  return userFollowerRouter;
+};
+
+export { initPodcastFollowersApi };
