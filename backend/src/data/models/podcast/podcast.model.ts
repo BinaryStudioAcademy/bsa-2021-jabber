6cfb622d5c4be0edf27a2a57dae7d4d10af234bc
yaroslav207 <yaroslav.sprynsian8@gmail.com>
2021-08-24 14:58:26 +0300

    j-245: fix after review

--- backend/src/data/models/podcast/podcast.model.ts
+++ backend/src/data/models/podcast/podcast.model.ts
@@ -23,7 +23,7 @@ class Podcast extends Abstract {
 
   [PodcastDTOKey.TYPE]: PodcastType;
 
-  [PodcastDTOKey.GENRE_ID]: number;
+  [PodcastDTOKey.GENRE_ID]: number | null;
 
   [PodcastDTOKey.USER]: User;
 
