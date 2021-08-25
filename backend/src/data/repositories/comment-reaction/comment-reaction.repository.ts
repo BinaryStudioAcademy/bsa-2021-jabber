701e455f7b4b97687334dc16f67920acaf1ea466
Andrii Solohub <andrei.sologub12@gmail.com>
2021-08-25 12:44:36 +0300

    J-237: + comment reaction repository

---
+++ backend/src/data/repositories/comment-reaction/comment-reaction.repository.ts
@@ -0,0 +1,42 @@
+import {
+  CommentReactionModel as CommentReactionM,
+} from '~/data/models/models';
+import {
+  CommentReaction as TCommentReaction,
+  CommentReactionCreatePayload,
+} from '~/common/types/types';
+import { comment } from '../repositories';
+
+type Constructor = {
+  CommentReactionModel: typeof CommentReactionM;
+};
+
+class CommentReaction {
+  #CommentReactionModel: typeof CommentReactionM;
+
+  constructor({ CommentReactionModel }: Constructor) {
+    this.#CommentReactionModel = CommentReactionModel;
+  }
+
+  public getByUserIdCommentId(payload: CommentReactionCreatePayload): Promise<TCommentReaction> {
+    return this.#CommentReactionModel.query()
+      .findOne({
+        'user_id': payload.userId,
+        'comment_id': payload.commentId,
+      });
+  }
+
+  public getCountByCommentId(commentId: number): Promise<number> {
+    return this.#CommentReactionModel.query()
+      .where('comment_id', commentId)
+      .resultSize();
+  }
+
+  public create(payload: CommentReactionCreatePayload): Promise<TCommentReaction> {
+    return this.#CommentReactionModel
+      .query()
+      .insert(payload);
+  }
+}
+
+export { CommentReaction };
