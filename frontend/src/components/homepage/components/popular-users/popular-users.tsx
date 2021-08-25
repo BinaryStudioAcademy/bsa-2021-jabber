47793a491ba1bd8a6f9a4ee9f8e56142f7e4b3bb
Yevhenii <eucheremisin@gmail.com>
2021-08-25 17:13:38 +0300

    J-263: *markup-for-popular-podcasters

--- frontend/src/components/homepage/components/popular-users/popular-users.tsx
+++ frontend/src/components/homepage/components/popular-users/popular-users.tsx
@@ -1,22 +1,35 @@
-
 import { User } from 'common/types/types';
 import { PopularUser } from './common/components/components';
+import { getAllowedClasses } from 'helpers/helpers';
 import styles from './styles.module.scss';
 
 type Props = {
-  popularUsers: User[],
+  popularUsers: User[];
 };
 
 const PopularUsers: React.FC<Props> = ({ popularUsers }: Props) => {
-
   return (
     <div className={styles.root}>
       <h2 className={styles.title}>Popular podcaster</h2>
-      <ul className={styles.usersRow}>
-        {popularUsers.map((user) => (
-          <PopularUser key={user.id} user={user} />
-        ))}
-      </ul>
+      {popularUsers.length >= 5 ? (
+        <ul
+          className={getAllowedClasses(styles.usersRow, styles.groupPodcastes)}
+        >
+          {popularUsers.map((user) => (
+            <>
+              <PopularUser key={user.id} user={user} />
+            </>
+          ))}
+        </ul>
+      ) : (
+        <ul className={styles.usersRow}>
+          {popularUsers.map((user) => (
+            <>
+              <PopularUser key={user.id} user={user} />
+            </>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
