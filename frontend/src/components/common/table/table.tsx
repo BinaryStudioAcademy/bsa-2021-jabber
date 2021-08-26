a9cc607f9d836cdc4ae7908c6cf9aa1be2bc8839
yaroslav207 <75075327+yaroslav207@users.noreply.github.com>
2021-08-26 06:01:11 +0300

    J-257: + pagination in table component

--- frontend/src/components/common/table/table.tsx
+++ frontend/src/components/common/table/table.tsx
@@ -1,7 +1,8 @@
 import { useTable } from 'hooks/hooks';
-import { Column, useSortBy } from 'react-table';
+import { Column, useSortBy, usePagination } from 'react-table';
 import styles from './styles.module.scss';
 import { getAllowedClasses } from 'helpers/helpers';
+import { Pagination } from './components/components';
 
 type Props = {
   columns: Column[];
@@ -9,52 +10,81 @@ type Props = {
 };
 
 const Table: React.FC<Props> = ({ columns, data = [] }) => {
-  const tableInstance = useTable({
-    columns: columns as Column<Record<string, string>>[],
-    data: data as Record<string, string>[],
-  }, useSortBy);
+  const tableInstance = useTable(
+    {
+      columns: columns as Column<Record<string, string>>[],
+      data: data as Record<string, string>[],
+    },
+    useSortBy,
+    usePagination,
+  );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    pageOptions,
+    state: { pageIndex, pageSize },
+    setPageSize,
+    gotoPage,
+    previousPage,
+    nextPage,
+    canPreviousPage,
+    canNextPage,
+  } = tableInstance;
 
   return (
-    <table {...getTableProps()} className={styles.table}>
-      <thead>
-        {headerGroups.map((headerGroup, i) => (
-          <tr {...headerGroup.getHeaderGroupProps()} key={i}>
-            {headerGroup.headers.map((column, i) => (
-
-              <th {...column.getHeaderProps(column.getSortByToggleProps())} key={i}>
-                <span>{column.render('Header')}</span>
-                <span className={getAllowedClasses([styles.sortIndicator, column.isSorted
-                  ? column.isSortedDesc
-                    ? styles.desc
-                    : styles.asc
-                  : null])
-                }>
-                </span>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()} key={i} className={styles.episodeRow}>
-              {row.cells.map((cell, i) => {
-                return (
-                  <td {...cell.getCellProps()} key={i}>
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
+    <div className={styles.tableWrapper}>
+      <table {...getTableProps()} className={styles.table}>
+        <thead>
+          {headerGroups.map((headerGroup, i) => (
+            <tr {...headerGroup.getHeaderGroupProps()} key={i}>
+              {headerGroup.headers.map((column, i) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())} key={i}>
+                  <span>{column.render('Header')}</span>
+                  <span className={getAllowedClasses([styles.sortIndicator, column.isSorted
+                    ? column.isSortedDesc
+                      ? styles.desc
+                      : styles.asc
+                    : null])
+                  }>
+                  </span>
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} key={i} className={styles.episodeRow}>
+                {row.cells.map((cell, i) => {
+                  return (
+                    <td {...cell.getCellProps()} key={i}>
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        previousPage={previousPage}
+        canPreviousPage={canPreviousPage}
+        pageIndex={pageIndex}
+        gotoPage={gotoPage}
+        pageCount={pageOptions.length}
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+        nextPage={nextPage}
+        canNextPage={canNextPage}
+      />
+    </div>
   );
 };
 
