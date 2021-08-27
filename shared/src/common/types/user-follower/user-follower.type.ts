9ea96649d4716261bee6dbd23f54c555492851aa
JuliaDzyuba <60258687+JuliaDzyuba@users.noreply.github.com>
2021-08-27 15:44:27 +0300

    J-270: + added types/enums

--- shared/src/common/types/user-follower/user-follower.type.ts
+++ shared/src/common/types/user-follower/user-follower.type.ts
@@ -1,9 +1,12 @@
+import { User } from '../user/user.type';
+
 type UserFollower = {
   id: number;
   userId: number;
   followerId: number;
   createdAt: string;
   updatedAt: string;
+  follower: User;
 };
 
 export type { UserFollower };
