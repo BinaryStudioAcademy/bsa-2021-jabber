ac85b17ab712d0be4b7716e405bd08eb7f7a18fa
Andrii Solohub <andrei.sologub12@gmail.com>
2021-08-29 11:04:41 +0300

    J-273: + logic to show placeholder while there is no content

--- frontend/src/components/homepage/components/popular-users/popular-users.tsx
+++ frontend/src/components/homepage/components/popular-users/popular-users.tsx
@@ -9,19 +9,26 @@ type Props = {
 };
 
 const PopularUsers: React.FC<Props> = ({ popularUsers }: Props) => {
+  const hasPopularUsers = Boolean(popularUsers.length);
   return (
     <div className={styles.root}>
       <h2 className={styles.title}>Popular podcaster</h2>
-      <ul
-        className={getAllowedClasses(
-          styles.usersRow,
-          popularUsers.length >= MAXIMUM_PODCASTERS_WITHOUT_MARKUP && styles.groupPodcastes,
-        )}
-      >
-        {popularUsers.map((user) => (
-          <PopularUser key={user.id} user={user} />
-        ))}
-      </ul>
+      {hasPopularUsers ? (
+        <ul
+          className={getAllowedClasses(
+            styles.usersRow,
+            popularUsers.length >= MAXIMUM_PODCASTERS_WITHOUT_MARKUP && styles.groupPodcastes,
+          )}
+        >
+          {popularUsers.map((user) => (
+            <PopularUser key={user.id} user={user} />
+          ))}
+        </ul>
+      ) : (
+        <span className={styles.oopsMessage}>
+          Oops! There&apos;s nothing here
+        </span>
+      )}
     </div>
   );
 };
