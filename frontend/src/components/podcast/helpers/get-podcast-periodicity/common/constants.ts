ded45ba3b2093fd6cc29db9899b8805cf8ae2d55
Artem-Vasylevytskyi <artem.totality@gmail.com>
2021-09-01 20:58:11 +0300

    J-296: * fix-issues

---
+++ frontend/src/components/podcast/helpers/get-podcast-periodicity/common/constants.ts
@@ -0,0 +1,6 @@
+const ONE_DAY = 1;
+const ONE_WEEK = 1;
+const ONE_MONTH = 1;
+const MONTHS_IN_YEAR = 12;
+
+export { ONE_DAY, ONE_WEEK, ONE_MONTH, MONTHS_IN_YEAR };
--- frontend/src/components/podcast/helpers/get-podcast-periodicity/get-podcast-periodicity.helper.ts
+++ frontend/src/components/podcast/helpers/get-podcast-periodicity/get-podcast-periodicity.helper.ts
@@ -1,6 +1,12 @@
 import { DateFormatType } from 'common/enums/enums';
 import { Episode } from 'common/types/types';
 import {
+  ONE_DAY,
+  ONE_WEEK,
+  ONE_MONTH,
+  MONTHS_IN_YEAR,
+} from './common/constants';
+import {
   getDifferenceInDays,
   getDifferenceInWeeks,
   getDifferenceInMonths,
@@ -19,7 +25,7 @@ const getPodcastPeriodicity = (episodes: Episode[]): string => {
     (prev, next) => new Date(prev).getTime() - new Date(next).getTime(),
   );
 
-  const firstDate = uniqueSortedEpisodesDates[0];
+  const [firstDate] = uniqueSortedEpisodesDates;
   const lastDate =
     uniqueSortedEpisodesDates[uniqueSortedEpisodesDates.length - 1];
 
@@ -36,31 +42,31 @@ const getPodcastPeriodicity = (episodes: Episode[]): string => {
 
   const differenceInDays = getDifferenceInDays(estimatedPeriodicity, 0);
 
-  if (differenceInDays <= 1) {
+  if (differenceInDays <= ONE_DAY) {
     return 'Daily';
   }
 
   const differenceInWeeks = getDifferenceInWeeks(estimatedPeriodicity, 0);
 
-  if (differenceInWeeks < 1) {
+  if (differenceInWeeks < ONE_WEEK) {
     return `Every ${differenceInDays} days`;
   }
 
-  if (differenceInWeeks === 1) {
+  if (differenceInWeeks === ONE_WEEK) {
     return 'Weekly';
   }
 
   const differenceInMonths = getDifferenceInMonths(estimatedPeriodicity, 0);
 
-  if (differenceInMonths < 1) {
+  if (differenceInMonths < ONE_MONTH) {
     return `Every ${differenceInWeeks} weeks`;
   }
 
-  if (differenceInMonths === 1) {
+  if (differenceInMonths === ONE_MONTH) {
     return 'Monthly';
   }
 
-  if (differenceInMonths < 12) {
+  if (differenceInMonths < MONTHS_IN_YEAR) {
     return `Every ${differenceInMonths} months`;
   }
 
