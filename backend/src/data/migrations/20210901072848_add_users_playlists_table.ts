ba53619b27d183a2504374f91dde7a558ac8fee9
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-09-01 10:43:29 +0300

    J-295: + users-playlists-table

---
+++ backend/src/data/migrations/20210901072848_add_users_playlists_table.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex';
+import { getFormattedISODate } from '~/helpers/helpers';
+
+const TABLE_NAME = 'users_playlists';
+
+export async function up(knex: Knex): Promise<void> {
+  const dateNowISO = getFormattedISODate(new Date());
+
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments('id').primary();
+    table
+      .integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users');
+    table.string('name').notNullable();
+    table.dateTime('created_at').notNullable().defaultTo(dateNowISO);
+    table.dateTime('updated_at').notNullable().defaultTo(dateNowISO);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+}
