0c619a313858900ab12b8cdcdea3fc8260c93870
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-09-01 16:48:09 +0300

    J-295: + playlist-types

--- backend/src/common/types/types.ts
+++ backend/src/common/types/types.ts
@@ -60,3 +60,5 @@ export type {
   UserFavouriteEpisodePayload,
   UserFavouriteEpisodeResponse,
 } from './user-favourite-episode/user-favourite-episode';
+export type { Playlist, PlaylistPayload } from './playlist/playlist';
+export type { PlaylistEpisode, PlaylistEpisodePayload } from './playlist-episode/playlist-episode';
