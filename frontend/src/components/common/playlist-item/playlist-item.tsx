c365fe463a2421332c8db38c43b43e151cb8fd09
Andrii Solohub <andrei.sologub12@gmail.com>
2021-09-03 10:39:48 +0300

    J-330: + playlist item component

---
+++ frontend/src/components/common/playlist-item/playlist-item.tsx
@@ -0,0 +1,38 @@
+import { AppRoute } from 'common/enums/enums';
+import { Playlist as TPlaylist } from 'common/types/types';
+import { Link, ImageWrapper } from 'components/common/common';
+import styles from './styles.module.scss';
+
+type Props = {
+  playlist: TPlaylist;
+};
+
+const PlaylistItem: React.FC<Props> = ({ playlist }) => (
+  <li>
+    <div className={styles.wrapper}>
+      <Link to={`${AppRoute.PLAYLISTS}/${playlist.id}`} className={styles.imgLink}>
+        <ImageWrapper
+          width="100"
+          height="100"
+          loading="lazy"
+          alt={playlist.name}
+          label={playlist.name}
+          className={styles.imageWrapper}
+        />
+      </Link>
+      <div className={styles.mainInformation}>
+        <Link to={`${AppRoute.PLAYLISTS}/${playlist.id}`} className={styles.link}>
+          {playlist.name}
+        </Link>
+        <Link
+          to={`${AppRoute.USERS}/${playlist.userId}`}
+          className={styles.linkUser}
+        >
+          {playlist.user.nickname}
+        </Link>
+      </div>
+    </div>
+  </li>
+);
+
+export default PlaylistItem;
