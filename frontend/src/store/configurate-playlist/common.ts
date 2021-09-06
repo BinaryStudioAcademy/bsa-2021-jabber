6d00bfe20d36f0a54896069fc2ca5103f8dcb4fc
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-09-06 15:55:27 +0300

    J-365: * configurate-playlist-store

--- frontend/src/store/configurate-playlist/common.ts
+++ frontend/src/store/configurate-playlist/common.ts
@@ -1,5 +1,8 @@
 enum ActionType {
   CREATE_PLAYLIST = 'configurate-playlist/create-playlist',
+  EDIT_PLAYLIST = 'configurate-playlist/edit-playlist',
+  LOAD_PLAYLIST = 'configurate-playlist/load-playlist',
+  RESET_STATE = 'configurate-playlist/reset-state',
 }
 
 export { ActionType };
