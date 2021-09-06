6a7b8516a4a138e71723e5fcf6a260e8d7e21c1d
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-09-06 23:25:57 +0300

    J-368: + max-possible-id-const

--- backend/src/common/constants/constants.ts
+++ backend/src/common/constants/constants.ts
@@ -2,3 +2,4 @@ export { USER_PASSWORD_SALT_ROUNDS, USER_NEW_PASSWORD_LENGTH } from './user/user
 export { PODCAST_LOAD_LIMIT } from './podcast/podcast';
 export { POPULAR_EPISODE_LOAD_LIMIT } from './episode/episode';
 export { POPULAR_PLAYLIST_LOAD_LIMIT } from './playlist/playlist';
+export { MAX_POSSIBLE_ID } from './db/db';
--- backend/src/middlewares/check-params-is-valid/check-params-is-valid.middleware.ts
+++ backend/src/middlewares/check-params-is-valid/check-params-is-valid.middleware.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 import { HttpError } from '~/exceptions/exceptions';
 import { ErrorMessage, HttpCode } from '~/common/enums/enums';
+import { MAX_POSSIBLE_ID } from '~/common/constants/constants';
 
 const checkParamsIsValid = (): RequestHandler => {
   const handler: RequestHandler = (req, _res, next) => {
@@ -8,7 +9,7 @@ const checkParamsIsValid = (): RequestHandler => {
     const params = Object.values(req.params);
 
     params.forEach((param) => {
-      if (isNaN(Number(param))) {
+      if (isNaN(Number(param)) || Number(param) > MAX_POSSIBLE_ID) {
         return next(new HttpError({
           status: HttpCode.BAD_REQUEST,
           message: ErrorMessage.BAD_REQUEST,
