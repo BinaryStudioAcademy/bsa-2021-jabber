6617e9b1aa71de03c72ad0aed20983ee971464bb
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-09-07 16:14:41 +0300

    J-391: + delete-actions

--- frontend/src/store/playlist/common.ts
+++ frontend/src/store/playlist/common.ts
@@ -2,6 +2,7 @@ enum ActionType {
   LOAD_PLAYLIST = 'playlist/load-playlist',
   LOAD_PLAYLIST_EPISODES = 'playlist/load-playlist-episodes',
   DELETE_PLAYLIST = 'playlist/delete-playlist',
+  DELETE_EPISODE_FROM_PLAYLIST = 'playlist/delete-episode-from-playlist',
 }
 
 export { ActionType };
